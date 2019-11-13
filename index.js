f81f81aa5f8d0161f73e7563f7ba26b36f02607b
iammadab <iammadab@gmail.com>
2019-11-13 17:29:35 +0100

    Removed the dynamic page serving, it was causing problems

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set("view engine", "ejs")
 app.use(express.static(path.resolve(__dirname, "public")))
 
 const routes = require("./routes")
-app.use(routes)
+app.use("/", routes)
 
 
 
@@ -17,9 +17,9 @@ app.use(routes)
 
 
 
-app.get("/:page", (req, res) => {
-	res.render(req.params.page, { link: "", title: "My Home" })
-})
+// app.get("/:page", (req, res) => {
+// 	res.render(req.params.page, { link: "", title: "My Home" })
+// })
 
 app.listen(3000, () => {
 	console.log("Application listening at port 3000")
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const appRouter = express.Router()
 
+const authRouter = require("./auth.route.js")
+console.log(authRouter)
 
+appRouter.use("/auth", authRouter)
 
 module.exports = appRouter
