d77b676d381c0e2dd26e4f0101120e5a14b2a527
iammadab <iammadab@gmail.com>
2019-11-15 17:22:20 +0100

    Created the login route"

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -6,5 +6,6 @@ const { bodyResponder } = require("./serviceAdapter")
 const userService = require("../services/user")
 
 authRouter.post("/", bodyResponder(userService.createUser))
+authRouter.post("/login", bodyResponder(userService.loginUser))
 
 module.exports = authRouter
