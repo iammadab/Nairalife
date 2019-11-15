423af306848bd06e1e151ebe6fff18f999e22ea8
iammadab <iammadab@gmail.com>
2019-11-15 11:38:04 +0100

    Changed the create user function to async form

--- services/user/user.create.js
+++ services/user/user.create.js
@@ -1,13 +1,17 @@
 const { createValidator } = require("lazy-validator")
+const userDb = require("../../data/db/user.db")
 
 const createUserValidator = createValidator("fullname.string.lowercase, phone.number, email.string.lowercase, password.string")
 
-function createUser(data){
+async function createUser(data){
 	let validationResult = createUserValidator.parse(data)
 	if(validationResult.error)
 		return { status: 400, code: "BAD_REQUEST_BODY", errors: validationResult.errors }
 
 	let userData = validationResult.data
+	let userObj = await userDb.createUser(userData)
+	console.log(userObj)
+	
 }
 
 module.exports = createUser
