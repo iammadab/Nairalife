5b74562f324a9b9f3ac0d216f0d4b9459edb6d2a
iammadab <iammadab@gmail.com>
2019-11-15 17:32:17 +0100

    Fixed bug in login user validation

--- services/user/user.login.js
+++ services/user/user.login.js
@@ -1,10 +1,11 @@
 const { createValidator } = require("lazy-validator")
+const userDb = require("../../data/db/user.db")
 
 const loginUserValidator = createValidator("phone.number, password.string")
 
 async function loginUser(data){
 	let validationResult = loginUserValidator.parse(data)
-	if(validationResult)
+	if(validationResult.error)
 		return { status: 400, code: "BAD_REQUEST_BODY", errors: validationResult.errors }
 
 	let loginData = validationResult.data
