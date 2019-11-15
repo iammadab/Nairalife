d62e4f3f1b13bfd627558d40d2917ba28a541587
iammadab <iammadab@gmail.com>
2019-11-15 10:46:08 +0100

    Created a service adapter file that has functions to let me adapt my service functions to http functions

---
+++ services/serviceAdapter.js
@@ -0,0 +1,19 @@
+function createResponder(type){
+	return function responder(fn){
+		return async function(req, res){
+			let response = await fn(req[type])
+			return sendResponse(res, response)
+		}
+
+		function sendResponse(res, responseData){
+			if(!responseData.status)
+				responseData.status = 500
+			res.status(responseData.status).json(responseData)
+		}
+	}
+}
+
+module.exports = {
+	bodyResponder: createResponder("body")
+	paramResponder: createResponder("params")
+}
