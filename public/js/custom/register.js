d0d976fb7ecc690051ce9ac50c013d08b5ac1448
iammadab <iammadab@gmail.com>
2019-11-16 17:33:12 +0100

    Showing custom error for missing fields

--- public/js/custom/register.js
+++ public/js/custom/register.js
@@ -1,15 +1,20 @@
 let store = {
 	registerButton: document.querySelector(".register-button"),
-	registerFormTag: ".register-form"
+	registerFormTag: ".register-form",
+	registrationInputs: Array.from(document.querySelectorAll(".register-form input"))
 }
 
 ;(function attachEvents(){
 	addEvent([store.registerButton], "click", startRegistration)
+	addEvent(store.registrationInputs, "input,focus", () => hideError("register-error"))
 })()
 
 function startRegistration(event){
 	event.preventDefault()
 	let userDetails = extractForm(store.registerFormTag)
-	console.log(userDetails)
+	let missingKeys = hasKeys(userDetails, ["fullname", "phone", "password"])
+
+	if(missingKeys.length > 0)
+		return showError("register-error", `You didn't fill data for ${missingKeys[0]}`)
 }
 
