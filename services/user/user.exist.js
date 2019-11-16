138178148d1064e2c7d3b7d26ec6e6553af0373c
iammadab <iammadab@gmail.com>
2019-11-16 11:00:36 +0100

    Generating new search query based on acceptable fields

--- services/user/user.exist.js
+++ services/user/user.exist.js
@@ -1,5 +1,15 @@
-async function userExist(){
+// const userDb = require("../")
+async function userExist(data){
+	let acceptableKeys = ["fullname", "phone", "email"]
+	let searchQuery = {}
 
+	Object.keys(data).forEach(key => {
+		if(acceptableKeys.includes(key))
+			searchQuery[key] = data[key]
+	})
+
+	if(Object.keys(searchQuery).length < 1)
+		return { status: 400, code: "NO_ACCEPTABLE_KEY", message: `Acceptable keys are: ${acceptableKeys.join(", ")}` }
 }
 
 module.exports = userExist
