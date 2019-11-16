2137df85e92ad5f02553fbdf33d9745bc9c33a39
iammadab <iammadab@gmail.com>
2019-11-16 10:52:06 +0100

    Linked the user router

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const appRouter = express.Router()
 
 const authRouter = require("./auth.route")
 const otpRouter = require("./otp.route")
+const userRouter = require("./user.route")
 
 appRouter.use("/auth", authRouter)
 appRouter.use("/otp", otpRouter)
+appRouter.use("/user", userRouter)
 
 module.exports = appRouter
