e002139ab11495ddac6168fe2d128325457293aa
iammadab <iammadab@gmail.com>
2019-11-16 09:20:16 +0100

    Implemented logic for otp creation

--- services/authentication/authentication.createOtp.js
+++ services/authentication/authentication.createOtp.js
@@ -1,4 +1,5 @@
 const { createValidator } = require("lazy-validator")
+const otpDb = require("../../data/db/otp.db")
 
 const createOtpValidator = createValidator("phone.number")
 
@@ -6,12 +7,17 @@ async function createOtp(data){
 	let validationResult = createOtpValidator.parse(data)
 	if(validationResult.error)
 		return { status: 400, code: "BAD_REQUEST_BODY", errors: validationResult.errors }
+
+	let otpObj = await otpDb.create({ phone: data.phone, code: generateCode() })
+
+	if(otpObj)
+		return { status: 200, code: "OTP_CREATED" }
 }
 
 module.exports = createOtp
 
 
 
-function generateOtp(){
+function generateCode(){
 	return (Math.floor((Math.random() * 90000)) + 10000)
 }
