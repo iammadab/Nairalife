23b34201f3316a962568f45c5dadf2eeae2e89d1
iammadab <iammadab@gmail.com>
2019-11-16 09:05:34 +0100

    Applied validation and norminalization to the verify otp service function

--- services/authentication/authentication.verifyOtp.js
+++ services/authentication/authentication.verifyOtp.js
@@ -1,5 +1,11 @@
-async function verifyOtp(){
+const { createValidator } = require("lazy-validator")
 
+const verifyOtpValidator = createValidator("phone.number, otp.number")
+
+async function verifyOtp(data){
+	let validationResult = verifyOtpValidator.parse(data)
+	if(validationResult.error)
+		return { status: 400, code: "BAD_REQUEST_BODY", errors: validationResult.errors }
 }
 
 module.exports = verifyOtp
