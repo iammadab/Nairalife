e25194016cec9457cde404b7eaabe004c979b322
iammadab <iammadab@gmail.com>
2019-11-16 08:56:16 +0100

    Created routes for otp creation and verification

--- routes/otp.route.js
+++ routes/otp.route.js
@@ -1,6 +1,11 @@
 const express = require("express")
 const otpRouter = express.Router()
 
+const { bodyResponder } = require("./serviceAdapter")
 
+const authenticationService = require("../services/authentication")
+
+otpRouter.post("/create", bodyResponder(authenticationService.createOtp))
+otpRouter.post("/verify", bodyResponder(authenticationService.verifyOtp))
 
 module.exports = otpRouter
