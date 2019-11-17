cb9a0e0e64f58d47384f349f9967afef1eb5b956
iammadab <iammadab@gmail.com>
2019-11-17 10:57:00 +0100

    Created the dynamic router service file and function to map stages to routes

---
+++ services/authentication/authentication.dynamicRouter.js
@@ -0,0 +1,14 @@
+function dynamicRouter(){
+
+}
+
+module.exports = dynamicRouter
+
+
+function mapStageToPage(stage){
+	let map = {
+		enter_card_details: "/account",
+		enter_contribution_preference: "/about",
+		active: "/home"
+	}
+}
