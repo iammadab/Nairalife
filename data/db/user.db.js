7c1a661c27f25b8287c4a0b7c91636e6b9779087
iammadab <iammadab@gmail.com>
2019-11-17 23:55:09 +0100

    Allowing for user id field during user creation

--- data/db/user.db.js
+++ data/db/user.db.js
@@ -2,12 +2,13 @@ const createDb = require("./base.db")
 const userModel = require("../models/user.model")
 const userDb = createDb(userModel)
 
-userDb.createUser = function({ fullname, phone, email, password }){
+userDb.createUser = function({ fullname, phone, email, password, user_id }){
 	let newUser = new userModel({
 		fullname,
 		phone,
 		email,
-		password
+		password,
+		user_id
 	})
 
 	return newUser.save()
