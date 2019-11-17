75c614d5bd2a98d78b6299d5aeb8b60a2f798da4
iammadab <iammadab@gmail.com>
2019-11-17 16:34:53 +0100

    Added validation for the user preference section

--- services/user/user.preference.js
+++ services/user/user.preference.js
@@ -1,5 +1,11 @@
+const { createValidator } = require("lazy-validator")
+
+const userPreferenceValidator = createValidator("sex.string.lowercase, relationship.string.lowercase, title.string.lowercase, bio.string.lowercase, work.string.lowercase, work_description.string.lowercase, earning.string.lowercase, contribution_receive.string.lowercase, contribtuion_make.string.lowercase, contribution_use.string.lowercase")
+
 async function userPreference(data){
-	
+	const userPreferenceValidationResult = userPreferenceValidator.parse(data)
+	if(userPreferenceValidationResult.error)
+		return { status: 400, code: "BAD_REQUEST_ERROR", errors: userPreferenceValidationResult.errors }
 }
 
 module.exports = userPreference
