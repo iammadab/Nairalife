5a866af180b361250890fe2446feef67b09978c1
iammadab <iammadab@gmail.com>
2019-11-17 18:01:20 +0100

    Hiding the missing error when an input or select is in focus

--- public/js/custom/about.js
+++ public/js/custom/about.js
@@ -1,14 +1,18 @@
 let store = {
 	preferenceButton: document.querySelector(".preference-submit"),
-	preferenceFormTag: ".preference-form"
+	preferenceFormTag: ".preference-form",
+	inputs: Array.all(document.querySelectorAll(".preference input, .preference textarea, .preference select"))
 }
 
 ;(function attachEvents(){
 	addEvent([store.preferenceButton], "click", submitPreference)
+	addEvent(store.inputs, "input,focus", () => hideError("preference-error"))
 })()	
 
 function submitPreference(event){
 	event.preventDefault()
 	let preferenceDetails = extractForm(store.preferenceFormTag)
 	let missingDetails = hasKeys(preferenceDetails, ["sex"])
+	if(missingDetails.length > 0)
+		return showError("preference-error", `You didn't fill a value for ${missingDetails[0]}`)
 }
