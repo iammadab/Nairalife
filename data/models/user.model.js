d48184179e9a6c150bdf926bdcde4a4560494c5f
iammadab <iammadab@gmail.com>
2019-11-17 18:43:04 +0100

    Added the new data to the user schema

--- data/models/user.model.js
+++ data/models/user.model.js
@@ -5,7 +5,13 @@ const userSchema = mongoose.Schema({
 	phone: { type: Number, required: true },
 	email: { type: String, required: true },
 	password: { type: String, required: true },
-	stage: { type: String, default: "enter_contribution_preference", enum: ["enter_card_details", "enter_contribution_preference", "active" ]}
+	stage: { type: String, default: "enter_contribution_preference", enum: ["enter_card_details", "enter_contribution_preference", "active" ]},
+
+	nairascore: { type: Number, default: 0 },
+	nairapoints: { type: Number, default: 0 },
+	guarantor_code: { type: Number, required: true, unique: true },
+	balance: { type: Number, default: 0 }
+
 })
 
 const userModel = mongoose.model("User", userSchema)
