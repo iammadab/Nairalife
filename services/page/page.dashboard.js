33d20357b9df10068322048e8919d4fb668669c3
iammadab <iammadab@gmail.com>
2019-11-18 00:14:11 +0100

    Implemented the dashboard routine

--- services/page/page.dashboard.js
+++ services/page/page.dashboard.js
@@ -1,5 +1,12 @@
-async function dashboard(req, res, next){
+const userDb = require("../../data/db/user.db")
 
+async function dashboard(req, res, next){
+	console.log(req)
+	
+	let userObj = await userDb.findOneWith({ phone: req.user })
+	if(userObj)
+		req.pageData.user = userObj
+	next()
 }
 
 module.exports = dashboard
