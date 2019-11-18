0790cd4523ef24fb1ed7eac9f912d9f1d515aa67
iammadab <iammadab@gmail.com>
2019-11-18 18:18:47 +0100

    Added a success alert

--- public/js/custom/settings.js
+++ public/js/custom/settings.js
@@ -15,4 +15,13 @@ function saveInformation(event){
 	let missingKeys = hasKeys(profileDetails, ["fullname", "phone", "email", "sex", "title", "relationship", "bio"])
 	if(missingKeys.length > 0)
 		return showError("profile-error", `You didn't fill data for ${missingKeys[0]}`)
+
+	return api("user/profile", { ...profileDetails, token: getToken() })
+			.then(handleResponse)
+
+	function handleResponse(response){
+		console.log(response)
+		if(response.status == 200)
+			console.log("Yay")
+	}
 }
