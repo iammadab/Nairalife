0c23f76d5b094667a8432aea3be28c1ae4ff3c26
iammadab <iammadab@gmail.com>
2019-11-18 19:47:29 +0100

    Apply validation to the user service route

--- services/user/user.changePassword.js
+++ services/user/user.changePassword.js
@@ -1,5 +1,11 @@
-async function changePassword(data){
+const { createValidator } = require("lazy-validator")
 
+const changePasswordValidator = createValidator("oldPassword.string, newPassword.string")
+
+async function changePassword(data){
+	let validationResult = changePasswordValidator.parse(data)
+	if(validationResult.error)
+		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
 }
 
 module.exports = changePassword
