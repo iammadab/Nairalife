93b7a712b4007a4b4de6dbd9cf3b6524910874a6
iammadab <iammadab@gmail.com>
2019-11-18 19:50:52 +0100

    Getting the user information based on the token data

--- services/user/user.changePassword.js
+++ services/user/user.changePassword.js
@@ -2,12 +2,15 @@ const { createValidator } = require("lazy-validator")
 
 const changePasswordValidator = createValidator("oldPassword.string, newPassword.string")
 
+const userDb = require("../../data/db/user.db")
+
 async function changePassword(data){
 	let validationResult = changePasswordValidator.parse(data)
 	if(validationResult.error)
 		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
 
-	
+	let userObj = await userDb.findOneWith({ _id: data.user.id })
+	console.log(userObj)
 }
 
 module.exports = changePassword
