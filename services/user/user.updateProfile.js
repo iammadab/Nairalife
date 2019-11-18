6cacc4f3ee6e2bae1d0b7eab4a80cd033e0f01d2
iammadab <iammadab@gmail.com>
2019-11-18 17:57:14 +0100

    User update functionality working from the api end

--- services/user/user.updateProfile.js
+++ services/user/user.updateProfile.js
@@ -2,10 +2,26 @@ const { createValidator } = require("lazy-validator")
 
 const updateProfileValidator = createValidator("fullname.string.lowercase, sex.string.lowercase, title.string.lowercase, relationship.string.lowercase, bio.string.lowercase")
 
+const userDb = require("../../data/db/user.db")
+
 async function updateProfile(data){
 	let validationResult = updateProfileValidator.parse(data)
 	if(validationResult.error)
 		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
+
+	let userObj = await userDb.findOneWith({ _id: data.user.id })
+	let accepted = ["sex", "title", "relationship", "bio"], update = {}
+	accepted.forEach(prop => {
+		if(data[prop])
+			update[prop] = data[prop]
+	})
+
+	let about = Object.assign({}, userObj._doc.about, update)
+	userObj = await userDb.appendDoc({ _id: data.user.id }, "fullname", data.fullname)
+	userObj = await userDb.appendDoc({ _id: data.user.id }, "about", about)
+	
+	if(userObj)
+		return { status: 200, code: "UPDATED_USER_PROFILE" }
 }
 
 module.exports = updateProfile
