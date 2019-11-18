18cedc69cfd8fef4d77cf21c8f09290a1477e86c
iammadab <iammadab@gmail.com>
2019-11-18 02:45:43 +0100

    Overwritten the findOne and findOneWith functions for users to stop showing the password in user db

--- services/page/page.dashboard.js
+++ services/page/page.dashboard.js
@@ -2,6 +2,7 @@ const userDb = require("../../data/db/user.db")
 
 async function dashboard(req, res, next){
 	let userObj = await userDb.findOneWith({ phone: req.body.user.phone })
+	console.log(userObj)
 	req.body.pageData = { 
 		user: userObj 
 	}
