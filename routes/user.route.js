913417fda2a4b2c1ae7b5ca759b6b04d7d65afdf
iammadab <iammadab@gmail.com>
2019-11-18 19:43:47 +0100

    Created the route to change password and linked it to the service function

--- routes/user.route.js
+++ routes/user.route.js
@@ -9,5 +9,6 @@ const authenticationService = require("../services/authentication")
 userRouter.post("/exist", bodyResponder(userService.userExist))
 userRouter.post("/preference", authenticationService.verifyToken, bodyResponder(userService.userPreference))
 userRouter.post("/profile", authenticationService.verifyToken, bodyResponder(userService.updateProfile))
+userRouter.post("/password", bodyResponder(userService.changePassword))
 
 module.exports = userRouter
