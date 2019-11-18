69dcc8824fce0e7ee4fb0c89756a939cef0f60be
iammadab <iammadab@gmail.com>
2019-11-18 19:42:46 +0100

    Created the change password service file

--- services/user/index.js
+++ services/user/index.js
@@ -3,13 +3,15 @@ const loginUser = require("./user.login")
 const userExist = require("./user.exist")
 const userPreference = require("./user.preference")
 const updateProfile = require("./user.updateProfile")
+const changePassword = require("./user.changePassword")
 
 let userService = {
 	createUser,
 	loginUser,
 	userExist,
 	userPreference,
-	updateProfile
+	updateProfile,
+	changePassword
 }
 
 module.exports = userService
---
+++ services/user/user.changePassword.js
@@ -0,0 +1,5 @@
+async function changePassword(data){
+
+}
+
+module.exports = changePassword
