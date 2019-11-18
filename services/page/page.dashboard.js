99480265616469572659f697d3aad4ec9ed8b7ba
iammadab <iammadab@gmail.com>
2019-11-18 03:12:37 +0100

    Works

--- services/page/page.dashboard.js
+++ services/page/page.dashboard.js
@@ -2,7 +2,6 @@ const userDb = require("../../data/db/user.db")
 
 async function dashboard(req, res, next){
 	let userObj = await userDb.findOneWith({ _id: req.body.user.id })
-	console.log(userObj)
 	req.body.pageData = { 
 		user: userObj 
 	}
