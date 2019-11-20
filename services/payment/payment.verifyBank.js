8c2594509548a0071d831dac6a4e97045c51d26a
iammadab <iammadab@gmail.com>
2019-11-20 15:57:56 +0100

    Populated the handle success function

--- services/payment/payment.verifyBank.js
+++ services/payment/payment.verifyBank.js
@@ -12,16 +12,30 @@ async function verifyBank(data){
 	if(validationResult.error)
 		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
 
-	verifyAccountNumber(data.account_number, data.bank_code)
+	let verificationResult = await verifyAccountNumber(data.account_number, data.bank_code)
+	if(verificationResult.status != 200)
+		return verificationResult
 
 }
 
-async function verifyAccountNumber(account_number, bank_code){
-	return axios(`https://api.paystack.co/bank/resolve?account_number=${account_number}&bank_code=${bank_code}`)
-			.then(handleResponse)
 
-	function handleResponse(response){
-		console.log(response)
+
+
+async function verifyAccountNumber(account_number, bank_code){
+	return axios(`https://api.paystack.co/bank/resolve?account_number=${account_number}&bank_code=${bank_code}`, requestOptions)
+			.then(handleSuccess)
+			.catch(handleFailure)
+
+	function handleSuccess(response){
+		console.log(response.data)
+		if(response.data.status == true)
+			return { status: 200, code: "ACCOUNT_VERIFICATION_SUCCESSFUL", data: response.data.data }
+	}
+
+	function handleFailure(response){
+		console.log(response.response.data)
+		if(response.response.data.status == false)
+			return { status: response.response.status, code: "ACCOUNT_VERIFICATION_FAILED", message: response.response.data.message }
 	}
 }
 module.exports = verifyBank
