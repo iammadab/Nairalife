38d2aa8685ebfca711e00040dc271d0fa2cd29e8
iammadab <iammadab@gmail.com>
2019-11-20 15:27:30 +0100

    Added the bank router to the application

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const appRouter = express.Router()
 const authRouter = require("./auth.route")
 const otpRouter = require("./otp.route")
 const userRouter = require("./user.route")
+const bankRouter = require("./bank.route")
 
 appRouter.use("/auth", authRouter)
 appRouter.use("/otp", otpRouter)
 appRouter.use("/user", userRouter)
+appRouter.use("/bank", bankRouter)
 
 module.exports = appRouter
