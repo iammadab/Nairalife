aab3f07e210b026154f21715ae29564e7d1c1727
iammadab <iammadab@gmail.com>
2019-11-20 09:56:31 +0100

    Applied the validator to forgot password

--- services/user/user.forgotPassword.js
+++ services/user/user.forgotPassword.js
@@ -1,5 +1,14 @@
-async function forgotPassword(data){
+const { createValidator } = require("lazy-validator")
 
+const forgotPasswordValidator = createValidator("code.number, password.string")
+
+const userDb = require("../../data/db/user.db")
+const otpDb = require("../../data/db/otp.db")
+
+async function forgotPassword(data){
+	let validationResult = forgotPasswordValidator.parse(data)
+	if(validationResult.error)
+		return { status: 400, code: "BAD_REQUEST_BODY", errors: validationResult.errors }
 }
 
 module.exports = forgotPassword
