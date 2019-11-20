07ec55a2927603de4b05a58d4d8237f5f7f5f8de
iammadab <iammadab@gmail.com>
2019-11-20 10:01:11 +0100

    Making sure user exists on forgot password

--- services/user/user.forgotPassword.js
+++ services/user/user.forgotPassword.js
@@ -9,6 +9,10 @@ async function forgotPassword(data){
 	let validationResult = forgotPasswordValidator.parse(data)
 	if(validationResult.error)
 		return { status: 400, code: "BAD_REQUEST_BODY", errors: validationResult.errors }
+
+	let userObj = await userDb.findOneWith({ phone: data.phone })
+	if(!userObj)
+		return { status: 403, code: "USER_DOES_NOT_EXIST" }
 }
 
 module.exports = forgotPassword
