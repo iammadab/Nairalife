564e901792fbce8f4cbc1fada45450cc1e900ae4
iammadab <iammadab@gmail.com>
2019-11-20 15:30:18 +0100

    added validation and norminalization

--- services/payment/payment.verifyBank.js
+++ services/payment/payment.verifyBank.js
@@ -1,5 +1,11 @@
-async function verifyBank(data){
+const { createValidator } = require("lazy-validator")
 
+const verifyBankValidator = createValidator("account_number.string, bank_code.string, bvn.string")
+
+async function verifyBank(data){
+	let validationResult = verifyBankValidator.parse(data)
+	if(validationResult.error)
+		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
 }
 
 module.exports = verifyBank
