05cff45b1fe47d261f024bf2d424946290042bc2
iammadab <iammadab@gmail.com>
2019-11-20 15:42:45 +0100

    Created a function that hits paystack to verify account

--- services/payment/payment.verifyBank.js
+++ services/payment/payment.verifyBank.js
@@ -1,11 +1,27 @@
+const axios = require("axios")
 const { createValidator } = require("lazy-validator")
 
 const verifyBankValidator = createValidator("account_number.string, bank_code.string, bvn.string")
+
+let requestOptions = {
+	headers: { Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`}
+}
 
 async function verifyBank(data){
 	let validationResult = verifyBankValidator.parse(data)
 	if(validationResult.error)
 		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
+
+	verifyAccountNumber(data.account_number, data.bank_code)
+
 }
 
+async function verifyAccountNumber(account_number, bank_code){
+	return axios(`https://api.paystack.co/bank/resolve?account_number=${account_number}&bank_code=${bank_code}`)
+			.then(handleResponse)
+
+	function handleResponse(response){
+		console.log(response)
+	}
+}
 module.exports = verifyBank
