1dbdfaf775024ff1361d137131a330dcea7e9847
iammadab <iammadab@gmail.com>
2019-11-20 10:32:09 +0100

    Sending the api request to send the otp

--- public/js/custom/forgot.js
+++ public/js/custom/forgot.js
@@ -1,14 +1,25 @@
 let store = {
 	recoverPasswordButton: document.querySelector(".recover-password-button"),
-	phoneElementTag: ".phone-form"
+	phoneElementTag: ".phone-form",
+	phoneInputs: Array.from(document.querySelectorAll(".phone-form input"))
 }
 
 ;(function attachEvent(){
 	addEvent([ store.recoverPasswordButton ], "click", sendOtp)
+	addEvent(store.phoneInputs, "input,focus", () => hideAlert("phone-error"))
 })()
 
 function sendOtp(event){
 	event.preventDefault()
 	let phoneDetails = extractForm(store.phoneElementTag)
-	console.log(phoneDetails)
+	let missingDetails = hasKeys(phoneDetails, ["phone"])
+	if(missingDetails.length > 0)
+		showAlert("phone-error", `You didn't fill data for ${missingDetails[0]}`)
+
+	return api("otp/create", { phone: phoneDetails.phone, type: "reset" })
+			.then(handleResponse)
+
+	function handleResponse(response){
+		console.log(response)
+	}
 }
