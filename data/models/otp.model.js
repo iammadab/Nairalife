8aea58f161cfa97ecd24507cef17444c1f0d312f
iammadab <iammadab@gmail.com>
2019-11-20 09:28:57 +0100

    Added the type to the otp schema

--- data/models/otp.model.js
+++ data/models/otp.model.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose")
 
 const otpSchema = mongoose.Schema({
 	phone: { type: Number, required: true },
-	code: { type: Number, required: true }
+	code: { type: Number, required: true },
+	type: { type: "String", default: "register" }
 })
 
 const otpModel = mongoose.model("Otp", otpSchema)
