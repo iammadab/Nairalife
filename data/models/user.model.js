90b7531a6a40e69b72c344038f5e37a7abae5a19
iammadab <iammadab@gmail.com>
2019-11-20 15:12:33 +0100

    Created a bank and card array for the users

--- data/models/user.model.js
+++ data/models/user.model.js
@@ -5,7 +5,10 @@ const userSchema = mongoose.Schema({
 	phone: { type: Number, required: true },
 	email: { type: String, required: true },
 	password: { type: String, required: true },
-	stage: { type: String, default: "enter_contribution_preference", enum: ["enter_card_details", "enter_contribution_preference", "active" ]},
+	stage: { type: String, default: "enter_card_details", enum: ["enter_card_details", "enter_contribution_preference", "active" ]},
+
+	bank: { type: Array, default: [] },
+	card: { type: Array, default: [] },
 
 	user_id: { type: Number, required: true, unique: true },
 	nairascore: { type: Number, default: 0 },
