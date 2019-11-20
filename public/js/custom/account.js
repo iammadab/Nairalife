a5ec908f9c70d67f58ab15bd5e16c7cdd42cb183
iammadab <iammadab@gmail.com>
2019-11-20 18:04:23 +0100

    Listening for events from the verify account button

--- public/js/custom/account.js
+++ public/js/custom/account.js
@@ -1,1 +1,14 @@
-alert("In accounts")
+let store = {
+	verifyBankButton: document.querySelector(".verify-bank-button"),
+	bankFormTag: ".bank-form",
+	bankFormInputs: Array.from(document.querySelectorAll(".bank-form input, .bank-form select"))
+}
+
+;(function attachEvent(){
+	addEvent([store.verifyBankButton], "click", verifyBank)
+})()	
+
+function verifyBank(event){
+	event.preventDefault()
+	console.log("Verifying bank")
+}
