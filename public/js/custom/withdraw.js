5491b6976b7a2b70f625a47e8b9adfbe14b44ff8
iammadab <iammadab@gmail.com>
2019-11-21 17:06:59 +0100

    Handling the responses from the withdraw response

--- public/js/custom/withdraw.js
+++ public/js/custom/withdraw.js
@@ -16,4 +16,21 @@ function withdrawFunds(event){
 	let missingDetails = hasKeys(withdrawDetails, ["amount"])
 	if(missingDetails.length > 0)
 		return showAlert("withdraw-error", `You didn't fill data for ${missingDetails[0]}`)
+
+	return api("user/withdraw", { amount: withdrawDetails.amount, token: getToken() })
+			.then(handleResponse)
+
+	function handleResponse(response){
+		console.log(response)
+		if(response.status == 200)
+			showAlert("withdraw-success", "Withdraw was successfull")
+		else if(response.code == "NEGATIVE_AMOUNT")
+			showAlert("withdraw-error", "You entered a negative amount")
+		else if(response.code == "INSUFFICIENT_BALANCE")
+			showAlert("withdraw-error", "You don't have sufficient balance")
+		else if(response.code == "BANK_NOT_ADDED")
+			showAlert("withdraw-error", "You have not added your bank")
+		else
+			showAlert("withdraw-error", "Withdraw failed. Try again later or contact us")
+	}
 }
