dbd0a227da9ceb3bf0a234a22996a3449ada0e81
iammadab <iammadab@gmail.com>
2019-11-21 12:34:35 +0100

    Applied validation and norminalization to the verify card request

--- services/payment/payment.verifyCard.js
+++ services/payment/payment.verifyCard.js
@@ -3,7 +3,9 @@ const { createValidator } = require("lazy-validator")
 const verifyCardValidator = createValidator("reference.string")
 
 async function verifyCard(data){
-
+	let validationResult = verifyCardValidator.parse(data)
+	if(validationResult.error)
+		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
 }
 
 module.exports = verifyCard
