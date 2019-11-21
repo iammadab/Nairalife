d6832fc2a76d66a259c87da9f7aac8a002e19cea
iammadab <iammadab@gmail.com>
2019-11-21 22:26:12 +0100

    Fetching the list of pages for the settings service

--- services/page/page.settings.js
+++ services/page/page.settings.js
@@ -2,9 +2,13 @@ const pageFunctions = require("./functions")
 
 async function settings(req, res, next){
 	let userObj = await pageFunctions.fetchUser(req.body.user.id)
+	let banks = await pageFunctions.fetchBanks()
+
 	req.body.pageData = {
-		user: userObj
+		user: userObj,
+		banks: banks
 	}
+	
 	next()
 }
 
