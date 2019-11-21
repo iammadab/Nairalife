49f1fddea62673bf3a64aeca0cce7bc7f5b28c53
iammadab <iammadab@gmail.com>
2019-11-21 12:45:09 +0100

    Created scaffold for the paystack function that verifies the transaction

--- services/payment/payment.verifyCard.js
+++ services/payment/payment.verifyCard.js
@@ -1,11 +1,33 @@
+const axios = require("axios")
 const { createValidator } = require("lazy-validator")
 
 const verifyCardValidator = createValidator("reference.string")
+
+const requestOptions = {
+	headers: { Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY} `}
+}
 
 async function verifyCard(data){
 	let validationResult = verifyCardValidator.parse(data)
 	if(validationResult.error)
 		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
+
+	verifyTransaction(data.reference)
+
+}
+
+function verifyTransaction(reference){
+	return axios(`https://api.paystack.co/transaction/verify/${reference}`)
+			.then(handleSuccess)
+			.catch(handleFailure)
+
+	function handleSuccess(response){
+		console.log(response)
+	}
+
+	function handleFailure(response){
+		console.log(response)
+	}
 }
 
 module.exports = verifyCard
