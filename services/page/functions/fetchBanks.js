5e03ca381654fb541a6e1f10caa71433dc874cb0
iammadab <iammadab@gmail.com>
2019-11-21 22:07:43 +0100

    Implemented the fetch banks function

---
+++ services/page/functions/fetchBanks.js
@@ -0,0 +1,21 @@
+const axios = require("axios")
+
+let requestOptions = {
+	headers: { Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`}
+}
+
+async function fetchBanks(){
+	axios("https://api.paystack.co/bank", requestOptions)
+		.then(handleSuccess)
+		.then(handleFailure)
+
+	function handleSuccess(response){
+		return response.data.data
+	}
+
+	function handleFailure(response){
+		return []
+	}
+}
+
+module.exports = account
