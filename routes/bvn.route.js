207e9aec68317ab21cc8950368a369a9990e5b7c
iammadab <iammadab@gmail.com>
2019-11-21 22:58:20 +0100

    Created router for bvn

---
+++ routes/bvn.route.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const bvnRouter = express.Router()
+
+const { bodyResponder } = require("./serviceAdapter")
+
+const paymentService = require("../services/payment")
+const authenticationService = require("../services/authentication")
+
+bvnRouter.post("/verify", authenticationService.verifyToken, bodyResponder(paymentService.verifyBvn))
+
+module.exports = bvnRouter
