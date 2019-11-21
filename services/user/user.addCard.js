ebbec0ae51a658a0402f3c5e1dd6b85600f8d548
iammadab <iammadab@gmail.com>
2019-11-21 13:13:25 +0100

    Applying validation to the add card service

--- services/user/user.addCard.js
+++ services/user/user.addCard.js
@@ -1,5 +1,16 @@
-async function addCard(data){
+const { createValidator } = require("lazy-validator")
 
+const addCardValidator = createValidator("authorization.object")
+
+const userDb = require("../../data/db/user.db")
+
+async function addCard(data){
+	let validationResult = addCardValidator.parse(data)
+	if(validationResult.error)
+		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
+
+	// let userObj = await userDb.findOne({ _id: data.user.id })
+	// if()
 }
 
 module.exports = addCard
