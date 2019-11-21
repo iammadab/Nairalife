8849a7afb1b7c86e1d64421d76d6ae3257e9a190
iammadab <iammadab@gmail.com>
2019-11-21 17:01:09 +0100

    Hiding alert on input and focus

--- public/js/custom/withdraw.js
+++ public/js/custom/withdraw.js
@@ -6,10 +6,14 @@ let store = {
 
 ;(function attachEvents(){
 	addEvent([store.withdrawButton], "click", withdrawFunds)
+	addEvent(store.withdrawInputs, "input,focus", () => hideAlert("withdraw-error"))
+	addEvent(store.withdrawInputs, "input,focus", () => hideAlert("withdraw-success"))
 })()
 
 function withdrawFunds(event){
 	event.preventDefault()
 	let withdrawDetails = extractForm(store.withdrawFormTag)
 	let missingDetails = hasKeys(withdrawDetails, ["amount"])
+	if(missingDetails.length > 0)
+		return showAlert("withdraw-error", `You didn't fill data for ${missingDetails[0]}`)
 }
