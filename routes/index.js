0877abc6bb0fa490c14b4cc652f632955556aea6
iammadab <iammadab@gmail.com>
2019-11-21 22:59:15 +0100

    Linked the bvn route to the application

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,13 @@ const otpRouter = require("./otp.route")
 const userRouter = require("./user.route")
 const bankRouter = require("./bank.route")
 const cardRouter = require("./card.route")
+const bvnRouter = require("./bvn.route")
 
 appRouter.use("/auth", authRouter)
 appRouter.use("/otp", otpRouter)
 appRouter.use("/user", userRouter)
 appRouter.use("/bank", bankRouter)
 appRouter.use("/card", cardRouter)
+appRouter.use("/bvn", bvnRouter)
 
 module.exports = appRouter
