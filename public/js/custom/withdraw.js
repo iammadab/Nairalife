0ffd4c5654ccd5b1ad1bc5b5e7ee4bb27182b1dd
iammadab <iammadab@gmail.com>
2019-11-21 16:53:05 +0100

    Listening for withdraw event

--- public/js/custom/withdraw.js
+++ public/js/custom/withdraw.js
@@ -1,1 +1,11 @@
-alert("Hello withdraw")
+let store = {
+	withdrawButton: document.querySelector(".withdraw-button")
+}
+
+;(function attachEvents(){
+	addEvent([store.withdrawButton], "click", withdrawFunds)
+})()
+
+function withdrawFunds(event){
+	event.preventDefault()
+}
