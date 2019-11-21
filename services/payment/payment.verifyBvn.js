6fa664b5b6c02e1cfad904a11bbdaec5b784ec28
iammadab <iammadab@gmail.com>
2019-11-21 23:02:35 +0100

    Applying validation to the verify bvn route

--- services/payment/payment.verifyBvn.js
+++ services/payment/payment.verifyBvn.js
@@ -1,5 +1,11 @@
-async function verifyBvn(data){
+const { createValidator } = require("lazy-validator")
 
+const bvnValidator = createValidator("bvn.number")
+
+async function verifyBvn(data){
+	let validationResult = bvnValidator.parse(data)
+	if(validationResult.error)
+		return { status: 400, code: "BAD_REQUEST_CODE", errors: validationResult.errors }
 }
 
 module.exports = verifyBvn
