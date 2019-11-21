02646cfc56b1626da1417c0eac83d4a6a463f55f
iammadab <iammadab@gmail.com>
2019-11-21 15:24:05 +0100

    Implemented function to create transfer receipt

--- services/payment/payment.withdraw.js
+++ services/payment/payment.withdraw.js
@@ -1,6 +1,11 @@
+const axios = require("axios")
 const { createValidator } = require("lazy-validator")
 
 const withdrawValidator = createValidator("amount.number")
+
+const requestOptions = {
+	headers: { Authorization: `Bearer ${process.env.PAYSTACK_PUBLIC_KEY}`}
+}
 
 const userDb = require("../../data/db/user.db")
 
@@ -25,3 +30,27 @@ async function withdraw(data){
 }
 
 module.exports = withdraw
+
+
+function createReceipt(name, account_number, bank_code){
+	let data = {
+		type: "nuban", 
+		name,
+		account_number,
+		bank_code,
+		currency: "NGN",
+		description: "Nairalife withdrawal"
+	}
+
+	return axios.post("https://api.paystack.co/transferrecipient", data, requestOptions)
+				.then(handleSuccess)
+				.catch(handleFailure)
+
+	function handleSuccess(response){
+		console.log(response)
+	}
+
+	function handleFailure(response){
+		console.log(response)
+	}
+}
