16e074e90aceeb587efd2f07647a3b61e30e6c94
iammadab <iammadab@gmail.com>
2019-11-21 22:57:17 +0100

    Added verify bvn to the payment service

--- services/payment/index.js
+++ services/payment/index.js
@@ -1,9 +1,11 @@
 const verifyBank = require("./payment.verifyBank")
+const verifyBvn = require("./payment.verifyBvn")
 const verifyCard = require("./payment.verifyCard")
 const withdraw = require("./payment.withdraw")
 
 const paymentServices = {
 	verifyBank,
+	verifyBvn,
 	verifyCard,
 	withdraw
 }
