6c903723889ff77ea74db389451bdb4bd41ef616
iammadab <iammadab@gmail.com>
2019-11-21 12:29:10 +0100

    Added the card router to the main application

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ const authRouter = require("./auth.route")
 const otpRouter = require("./otp.route")
 const userRouter = require("./user.route")
 const bankRouter = require("./bank.route")
+const cardRouter = require("./card.route")
 
 appRouter.use("/auth", authRouter)
 appRouter.use("/otp", otpRouter)
 appRouter.use("/user", userRouter)
 appRouter.use("/bank", bankRouter)
+appRouter.use("/card", cardRouter)
 
 module.exports = appRouter
