d6bbe1999e1f91b25578cc27f8deefac08965e2e
iammadab <iammadab@gmail.com>
2019-11-21 15:03:59 +0100

    Preventing user from entering a negative amount for withdrawal

--- services/payment/payment.withdraw.js
+++ services/payment/payment.withdraw.js
@@ -6,6 +6,9 @@ async function withdraw(data){
 	let validationResult = withdrawValidator.parse(data)
 	if(validationResult.error)
 		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
+
+	if(data.amount < 0)
+		return { status: 403, code: "NEGATIVE_AMOUNT" }
 }
 
 module.exports = withdraw
