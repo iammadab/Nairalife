f29ec8d990f63fc598bf2467ab03bffb734da7ac
iammadab <iammadab@gmail.com>
2019-11-21 15:02:37 +0100

    Applying validation to the withdraw service

--- services/payment/payment.withdraw.js
+++ services/payment/payment.withdraw.js
@@ -1,5 +1,11 @@
-async function withdraw(data){
+const { createValidator } = require("lazy-validator")
 
+const withdrawValidator = createValidator("amount.number")
+
+async function withdraw(data){
+	let validationResult = withdrawValidator.parse(data)
+	if(validationResult.error)
+		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
 }
 
 module.exports = withdraw
