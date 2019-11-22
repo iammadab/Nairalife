8cd177df1921ebc4dceaf35b0c6fc8d7ecf6b6e4
iammadab <iammadab@gmail.com>
2019-11-22 22:23:37 +0100

    Applying validation to the group create section

--- services/group/group.create.js
+++ services/group/group.create.js
@@ -1,5 +1,11 @@
-async function createGroup(data){
+const { createValidator } = require("lazy-validator")
 
+const createGroupValidator = createValidator("group_title.string, group_description.string, group_goals.string, total_members.number, contribution_amount.number, contribution_period.string")
+
+async function createGroup(data){
+	let validationResult = createGroupValidator.parse(data)
+	if(validationResult.error)
+		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
 }
 
 module.exports = createGroup
