7fc165d8c987074d555c8d1d2a51b82721156832
iammadab <iammadab@gmail.com>
2019-11-23 00:22:50 +0100

    Added the groups page service to the page services

--- services/page/index.js
+++ services/page/index.js
@@ -1,11 +1,13 @@
 const dashboard = require("./page.dashboard")
 const settings = require("./page.settings")
 const account = require("./page.account")
+const groups = require("./page.groups")
 
 const pageServices = {
 	dashboard,
 	settings,
-	account
+	account,
+	groups
 }
 
 module.exports = pageServices
