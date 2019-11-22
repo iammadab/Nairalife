cfc22bd6ce1158387b50ebf08509271fed8dd88e
iammadab <iammadab@gmail.com>
2019-11-22 16:39:00 +0100

    Making sure the user exists

--- services/admin/admin.login.js
+++ services/admin/admin.login.js
@@ -2,10 +2,16 @@ const { createValidator } = require("lazy-validator")
 
 const adminLoginValidator = createValidator("phone.number, password.string")
 
+const userDb = require("../../data/db/user.db")
+
 async function loginAdmin(data){
 	let validationResult = adminLoginValidator.parse(data)
 	if(validationResult.error)
 		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
+
+	let userObj = await userDb.findOneWith({ phone: data.phone })
+	if(!userObj)
+		return { status: 403, code: "USER_DOES_NOT_EXIST" }
 }
 
 module.exports = loginAdmin
