37c98d9f5041b3a5f4a14ed4c559aa7adf60d6b1
iammadab <iammadab@gmail.com>
2019-11-22 22:09:37 +0100

    Created the schema for the group

--- data/models/group.model.js
+++ data/models/group.model.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose")
 
 const groupSchema = mongoose.Schema({
-
+	group_id: { type: Number, required: true },
+	group_title: { type: String, required: true },
+	group_description: { type: String, required: true },
+	group_goals: { type: String, required: true },
+	total_members: { type: Number, required: true },
+	contribution_amount: { type: Number, required: true },
+	contribution_period: { type: String, required: true, enum: ["daily", "weekly", "monthly"]}
 })
 
 const groupModel = mongoose.model("Group", groupSchema)
