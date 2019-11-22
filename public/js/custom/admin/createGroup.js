01800ae036bb988dacd88e4bc70dc4da9ad09974
iammadab <iammadab@gmail.com>
2019-11-22 23:28:22 +0100

    Added text area to complete group inputs

--- public/js/custom/admin/createGroup.js
+++ public/js/custom/admin/createGroup.js
@@ -1,3 +1,14 @@
 let store = {
-	
+	createGroupButton: document.querySelector(".create-group-button"),
+	createGroupFormTag: ".create-form",
+	createGroupInputs: document.querySelectorAll(".create-form input, .create-form select, .create-form textarea")
+}
+
+;(function attachEvents(){
+	addEvent([store.createGroupButton], "click", createGroup)
+})()
+
+function createGroup(event){
+	event.preventDefault()
+	console.log("Creating group")
 }
