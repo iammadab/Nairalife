595f1f3c51ccb836f398f2a584c00a7f604dc09f
iammadab <iammadab@gmail.com>
2019-11-22 16:37:06 +0100

    Apply validation to the admin login route

--- services/admin/admin.login.js
+++ services/admin/admin.login.js
@@ -1,5 +1,11 @@
-async function loginAdmin(data){
+const { createValidator } = require("lazy-validator")
 
+const adminLoginValidator = createValidator("phone.number, password.string")
+
+async function loginAdmin(data){
+	let validationResult = adminLoginValidator.parse(data)
+	if(validationResult.error)
+		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
 }
 
 module.exports = loginAdmin
