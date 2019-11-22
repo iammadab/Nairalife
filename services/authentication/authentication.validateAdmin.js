588d1c64737a437ce222926da62ec1bd1f65c2b2
iammadab <iammadab@gmail.com>
2019-11-22 23:15:31 +0100

    Implemented the api for group creation

--- services/authentication/authentication.validateAdmin.js
+++ services/authentication/authentication.validateAdmin.js
@@ -5,7 +5,7 @@ async function validateAdmin(req, res, next){
 	// Or we could fetch the user and check the database to make sure the user is actual an admin
 	// I will be choosing the later, as it is more secure, incase the key get's compromised
 	let userObj = await userDb.findOneWith({ _id: req.body.user.id })
-	console.log(userObj)
+	// console.log(userObj)
 	if(!userObj)
 		return res.status(403).json({ code: "USER_DOES_NOT_EXIST", status: 403 })
 	if(userObj.role != "admin")
