fbe0a9a64aab8c97791b21ae76c0130cc8f924a1
iammadab <iammadab@gmail.com>
2019-11-23 17:58:02 +0100

    Applied validation and norminalization to the delete member route

--- services/group/group.removeMember.js
+++ services/group/group.removeMember.js
@@ -1,5 +1,18 @@
+const { createValidator } = require("lazy-validator")
+
+const removeMemberValidator = createValidator("user_id.number, group_id.number")
+
+const userDb = require("../../data/db/user.db")
+const groupDb = require("../../data/db/group.db")
+
 async function removeMember(data){
-	
+	let validationResult = removeMemberValidator.parse(data)
+	if(validationResult.error)
+		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors } 
+
+	data = validationResult.data
+
+
 }
 
 module.exports = removeMember
