a8051bd8f08e557c77ab60628a5057de584233db
iammadab <iammadab@gmail.com>
2019-11-23 17:27:16 +0100

    Clearing the input on successfull add

--- public/js/custom/admin/addMember.js
+++ public/js/custom/admin/addMember.js
@@ -14,24 +14,25 @@ function addMember(event){
 	event.preventDefault()
 	console.log("Adding member")
 	let addDetails = extractForm(store.addMemberFormTag)
-	let missingDetails = hasKeys(addDetails, ["group_id", "user_id"])
+	let missingDetails = hasKeys(addDetails, ["user_id", "group_id"])
 	if(missingDetails.length > 0)
 		return showAlert("member-error", `You didn't fill data for ${missingDetails[0]}`)
 
 	return api("group/member", { token: getToken(), ...addDetails })
-			.then(handleresponse)
+			.then(handleResponse)
 
 	function handleResponse(response){
-		console.log(response)
-		if(response.status == 200)
+		if(response.status == 200){
 			showAlert("member-success", "Added user to group successfully")
+			store.addMemberInputs.forEach(input => input.value = "")
+		}
 		else if(response.code == "USER_DOES_NOT_EXIST")
-			showAlert("member-success", "No user found for that id")
+			showAlert("member-error", "No user found for that id")
 		else if(response.code == "GROUP_DOES_NOT_EXIST")
-			showAlert("member-success", "No group found for that id")
+			showAlert("member-error", "No group found for that id")
 		else if(response.code == "USER_HAS_GROUP")
-			showAlert("member-success", "The user already belongs to a group")
+			showAlert("member-error", "The user already belongs to a group")
 		else if(response.code == "PROBLEM_ADDING_USER")
-			showAlert("member-success", "Problem adding user. Contact support")
+			showAlert("member-error", "Problem adding user. Contact support")
 	}
 }
