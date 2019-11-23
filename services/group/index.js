fa8bc8737e30ae0042d55f9ee340875cdbdd4d00
iammadab <iammadab@gmail.com>
2019-11-23 17:52:32 +0100

    Added the remove member service function to the group services

--- services/group/index.js
+++ services/group/index.js
@@ -1,9 +1,11 @@
 const createGroup = require("./group.create")
 const addMember = require("./group.addMember")
+const removeMember = require("./group.removeMember")
 
 const groupServices = {
 	createGroup,
-	addMember
+	addMember,
+	removeMember
 }
 
 module.exports = groupServices
