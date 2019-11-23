28434f05f094355e5a5976fa2948918f448e821e
iammadab <iammadab@gmail.com>
2019-11-23 10:52:24 +0100

    Getting the group id from the url and sending it ot the group fetcher

---
+++ services/page/page.group.js
@@ -0,0 +1,9 @@
+const pageFunctions = require("./functions")
+
+async function group(req, res, next){
+	let group_id = req.params.group_id,
+		groupObj = await pageFunctions.findOneWith({ group_id })
+
+	console.log(groupObj)
+	next()
+}
