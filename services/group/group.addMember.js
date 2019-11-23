141202894ce03734c25608d13f651ca3e9a8925b
iammadab <iammadab@gmail.com>
2019-11-23 16:08:14 +0100

    Applying the add member validation

--- services/group/group.addMember.js
+++ services/group/group.addMember.js
@@ -3,7 +3,9 @@ const { createValidator } = require("lazy-validator")
 const addMemberValidator = createValidator("group_id.number, user_id.number")
 
 async function addMember(data){
-
+	let validationResult = addMemberValidator.parse(data)
+	if(validationResult.error)
+		return { status: 400, code: "BAD_REQUEST_CODE", errors: validationResult.errors }
 }
 
 module.exports = addMember
