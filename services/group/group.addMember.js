2ff9fc50abfa5d4a87252e1278bd0cb1fa6661e7
iammadab <iammadab@gmail.com>
2019-11-23 16:19:09 +0100

    Fetching the user and the group, reporting

--- services/group/group.addMember.js
+++ services/group/group.addMember.js
@@ -2,10 +2,21 @@ const { createValidator } = require("lazy-validator")
 
 const addMemberValidator = createValidator("group_id.number, user_id.number")
 
+const userDb = require("../../data/db/user.db")
+const groupDb = require("../../data/db/group.db")
+
 async function addMember(data){
 	let validationResult = addMemberValidator.parse(data)
 	if(validationResult.error)
 		return { status: 400, code: "BAD_REQUEST_CODE", errors: validationResult.errors }
+
+	let userObj = await userDb.findOneWith({ user_id: data.user_id })
+	if(!userObj)
+		return { status: 403, code: "USER_DOES_NOT_EXIST" }
+
+	let groupObj = await groupDb.findOneWith({ group_id: data.group_id })
+	if(!groupObj)
+		return { status: 403, code: "GROUP_DOES_NOT_EXIST" }
 }
 
 module.exports = addMember
