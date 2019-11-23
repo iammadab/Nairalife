af30713c5adf778e014dd0dedadc840fc2c93b23
iammadab <iammadab@gmail.com>
2019-11-23 11:41:34 +0100

    Hanlding the wrong group id, redirecting to the groups page

--- services/page/page.group.js
+++ services/page/page.group.js
@@ -2,9 +2,15 @@ const pageFunctions = require("./functions")
 
 async function group(req, res, next){
 	let group_id = req.params.group_id,
-		groupObj = await pageFunctions.findOneWith({ group_id })
+		groupObj = await pageFunctions.fetchGroup(group_id)
 
-	console.log(groupObj)
+	if(!groupObj)
+		return res.redirect("/admin/groups")
+
+	req.body.pageData = {
+		group: groupObj
+	}
+	
 	next()
 }
 
