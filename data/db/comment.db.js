f372fb588fc855c380be190d797bc319f0593e7c
iammadab <iammadab@gmail.com>
2019-11-24 11:23:21 +0100

    Fixed username instead of fullname in comments

--- data/db/comment.db.js
+++ data/db/comment.db.js
@@ -2,9 +2,9 @@ const createDb = require("./base.db")
 const commentModel = require("../models/comment.model")
 const commentDb = createDb(commentModel)
 
-commentDb.createComment = function({ fullname, comment, user_id, group_id }){
+commentDb.createComment = function({ username, comment, user_id, group_id }){
 	let newComment = new commentModel({
-		fullname,
+		username,
 		comment,
 		user_id,
 		group_id
