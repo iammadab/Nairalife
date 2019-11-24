dc4fa81fdd39f08a509e8e1d337b57bb4017e728
iammadab <iammadab@gmail.com>
2019-11-24 23:50:57 +0100

    Added the new cycle to the group services

--- services/group/index.js
+++ services/group/index.js
@@ -3,13 +3,15 @@ const addMember = require("./group.addMember")
 const removeMember = require("./group.removeMember")
 const addComment = require("./group.addComment")
 const startGroup = require("./group.start")
+const newCycle = require("./group.newCycle")
 
 const groupServices = {
 	createGroup,
 	addMember,
 	removeMember,
 	addComment,
-	startGroup
+	startGroup,
+	newCycle
 }
 
 module.exports = groupServices
