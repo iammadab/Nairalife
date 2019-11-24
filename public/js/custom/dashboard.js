5e8fdc48253c2a036ed3f0b3957c703288d74345
iammadab <iammadab@gmail.com>
2019-11-24 12:54:06 +0100

    Deleted the comments route

--- public/js/custom/dashboard.js
+++ public/js/custom/dashboard.js
@@ -9,5 +9,17 @@ let store = {
 
 function addComment(event){
 	event.preventDefault()
-	console.log("Adding comment")
+	let { group_id } = event.target.dataset
+	let commentData = document.querySelector(".comment-box").value
+	if(!commentData)
+		return console.log("You didn't add a comment")
+
+	return api("group/comment", { token: getToken(), group_id, comment: commentData })
+			.then(handleResponse)
+
+	function handleResponse(response){
+		// console.log(response)
+		if(response.status == 200)
+			redirect(window.location.href)
+	}
 }
