3cdc25c2c8203bc95306fccfb5bc6d62d5b664b8
iammadab <iammadab@gmail.com>
2019-11-24 19:24:49 +0100

    Applying validation to the group start service

--- services/group/group.start.js
+++ services/group/group.start.js
@@ -1,5 +1,11 @@
-async function startGroup(data){
+const { createValidator } = require("lazy-validator")
 
+const startGroupValidator = createValidator("group_id.number")
+
+async function startGroup(data){
+	let validationResult = startGroupValidator.parse(data)
+	if(validationResult.error)
+		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
 }
 
 module.exports = startGroup
