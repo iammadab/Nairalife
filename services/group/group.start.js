ed95fafcbe489a379d0e29cff382d72a445e5af6
iammadab <iammadab@gmail.com>
2019-11-24 20:35:37 +0100

    Removed the async part from the sort

--- services/group/group.start.js
+++ services/group/group.start.js
@@ -17,27 +17,26 @@ async function startGroup(data){
 	if(groupObj.status != "inactive")
 		return { status: 403, code: "GROUP_HAS_STARTED" }
 
-	let members = groupObj.members
-	console.log(members)
-	console.log("")
-	members.sort(orderFunction)
-	console.log(members)
+	let members = groupObj.members, membersMap = {}
+	for(let i = 0; i < members.length; i++){
+		membersMap[members[i].user_id] = await userDb.findOneWith({ user_id: members[i].user_id })
+	}
+	// console.log(members)
+	// console.log("")
+	// members.sort(orderFunction)
+	// console.log(members)
 
 
+	// if you return -1 a comes before b
+	// if you return 1 b comes before a
+	// if you return 0 they remain in their positions
+	async function orderFunction(a, b){
+		let userA = membersMap[a.user_id], userB = membersMap[b.user_id]
 
+		if(userA.nairascore > userB.nairascore) return -1
+		if(userA.nairascore > userB.nairascore) return 1
+		if(userA.nairascore == userB.nairascore) return 0
+	}
 }
 
 module.exports = startGroup
-
-// if you return -1 a comes before b
-// if you return 1 b comes before a
-// if you return 0 they remain in their positions
-async function orderFunction(a, b){
-	let userA = await userDb.findOneWith({ user_id: a.user_id })
-	let userB = await userDb.findOneWith({ user_id: b.user_id })
-
-	if(userA.nairascore > userB.nairascore) return -1
-	if(userA.nairascore > userB.nairascore) return 1
-	if(userA.nairascore == userB.nairascore) return 0
-		
-}
