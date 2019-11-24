7d491e324da227b4f7a377a0f9e01520880e8852
iammadab <iammadab@gmail.com>
2019-11-24 11:33:22 +0100

    Apply validation and norminalization to the group add comment service

--- services/group/group.addComment.js
+++ services/group/group.addComment.js
@@ -1,5 +1,11 @@
-async function addComment(data){
+const { createValidator } = require("lazy-validator")
 
+const addCommentValidator = createValidator("comment.string, group_id.number")
+
+async function addComment(data){
+	let validationResult = addCommentValidator.parse(data)
+	if(validationResult.error)
+		return { status: 400, code: "BAD_REQUEST_CODE", errors: validationResult.errors }
 }
 
 module.exports = addComment
