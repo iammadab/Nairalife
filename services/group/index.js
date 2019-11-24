e4c8ba3fdc09ca6bf4e0bd528943106a12ce768d
iammadab <iammadab@gmail.com>
2019-11-24 19:14:33 +0100

    Added the start group to the group services

--- services/group/index.js
+++ services/group/index.js
@@ -2,12 +2,14 @@ const createGroup = require("./group.create")
 const addMember = require("./group.addMember")
 const removeMember = require("./group.removeMember")
 const addComment = require("./group.addComment")
+const startGroup = require("./group.start")
 
 const groupServices = {
 	createGroup,
 	addMember,
 	removeMember,
-	addComment
+	addComment,
+	startGroup
 }
 
 module.exports = groupServices
