82809429b818e9cdaa2b392d32d64f25bb793945
iammadab <iammadab@gmail.com>
2019-11-24 13:04:11 +0100

    Rendering the comments latest first

--- services/page/functions/fetchComments.js
+++ services/page/functions/fetchComments.js
@@ -1,7 +1,7 @@
 const commentDb = require("../../../data/db/comment.db")
 
 async function fetchComments(querySpecifier){
-	return commentDb.findWith(querySpecifier)
+	return commentDb.findWith(querySpecifier, {}, { created_at: -1 })
 }
 
 module.exports = fetchComments
