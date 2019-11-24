199ad9204480ed977271d4015bce8e20b9cf16a9
iammadab <iammadab@gmail.com>
2019-11-24 11:18:23 +0100

    Created the db function to create comments

--- data/db/comment.db.js
+++ data/db/comment.db.js
@@ -2,4 +2,15 @@ const createDb = require("./base.db")
 const commentModel = require("../models/comment.model")
 const commentDb = createDb(commentModel)
 
+commentDb.createComment = function({ fullname, comment, user_id, group_id }){
+	let newComment = new commentModel({
+		fullname,
+		comment,
+		user_id,
+		group_id
+	})
+
+	return newComment.save()
+}
+
 module.exports = commentDb
