4625006d142736fac3acab900c79668ee962d5ec
iammadab <iammadab@gmail.com>
2019-11-24 19:34:42 +0100

    Added the group id and the user id

--- services/group/group.start.js
+++ services/group/group.start.js
@@ -2,10 +2,15 @@ const { createValidator } = require("lazy-validator")
 
 const startGroupValidator = createValidator("group_id.number")
 
+const groupDb = require("../../data/db/group.db")
+const userDb = require("../../data/db/user.db")
+
 async function startGroup(data){
 	let validationResult = startGroupValidator.parse(data)
 	if(validationResult.error)
 		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
+
+
 }
 
 module.exports = startGroup
