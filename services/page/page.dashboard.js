47b89ef2444de90766edb4a18f31876f683958f8
iammadab <iammadab@gmail.com>
2019-11-24 08:43:03 +0100

    Sending the full member details to the dashboard

--- services/page/page.dashboard.js
+++ services/page/page.dashboard.js
@@ -3,10 +3,18 @@ const pageFunctions = require("./functions")
 async function dashboard(req, res, next){
 	let userObj = await pageFunctions.fetchUser(req.body.user.id)
 	let groupObj = await pageFunctions.fetchGroup(userObj.group)
-	
+	let members = []
+
+	groupObj.members..forEach(async member => {
+		if(member.removed) return
+		let memberObj = await pageFunctions.fetchUser(member.user_id)
+		members.push(memberObj)
+	})
+
 	req.body.pageData = {
 		user: userObj,
-		group: groupObj
+		group: groupObj,
+		members
 	}
 
 	next()
