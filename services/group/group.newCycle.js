43f04ec3e909181827cfc53ffa624053af5eceac
iammadab <iammadab@gmail.com>
2019-11-25 00:20:51 +0100

    Creating the new group based on the old group data

--- services/group/group.newCycle.js
+++ services/group/group.newCycle.js
@@ -2,10 +2,33 @@ const { createValidator } = require("lazy-validator")
 
 const newCycleValidator = createValidator("group_id.number")
 
+const groupDb = require("../../data/db/group.db")
+
+const createGroup = require("./group.create")
+
 async function newCycle(data){
 	let validationResult = newCycleValidator.parse(data)
 	if(validationResult.error)
 		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
+
+	let groupObj = await groupDb.findOneWith({ group_id: data.group_id })
+	if(!groupObj)
+		return { status: 403, code: "GROUP_DOES_NOT_EXIST" }
+
+	// Creating the new group based on the data of the old group
+	let newGroupResult = await createGroup({
+		group_title: groupObj.group_title,
+		group_description: groupObj.group_description, 
+		group_goals: groupObj.group_goals,
+		total_members: groupObj.total_members,
+		contribution_amount: groupObj.contribution_amount,
+		contribution_period: groupObj.contribution_period
+	})
+	if(newGroupResult.status != 200)
+		return newGroupResult
+
+	let newGroupObj = newGroupResult.group
+
 }
 
 module.exports = newCycle
