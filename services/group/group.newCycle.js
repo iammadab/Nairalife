55f8658e49e13e67bb1e319c864603f6b05fd0c7
iammadab <iammadab@gmail.com>
2019-11-25 00:14:31 +0100

    Apply the validator to the new cycle service

--- services/group/group.newCycle.js
+++ services/group/group.newCycle.js
@@ -1,5 +1,11 @@
-async function newCycle(data){
+const { createValidator } = require("lazy-validator")
 
+const newCycleValidator = createValidator("group_id.number")
+
+async function newCycle(data){
+	let validationResult = newCycleValidator.parse(data)
+	if(validationResult.error)
+		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
 }
 
 module.exports = newCycle
