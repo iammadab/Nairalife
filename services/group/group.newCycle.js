b07eebed45b0f60e6e1a0e1e39260eb7820e10fe
iammadab <iammadab@gmail.com>
2019-11-25 00:40:46 +0100

    Checking if the user exists

--- services/group/group.newCycle.js
+++ services/group/group.newCycle.js
@@ -3,6 +3,7 @@ const { createValidator } = require("lazy-validator")
 const newCycleValidator = createValidator("group_id.number")
 
 const groupDb = require("../../data/db/group.db")
+const userDb = require("../../data/db/user.db")
 
 const createGroup = require("./group.create")
 
@@ -11,9 +12,14 @@ async function newCycle(data){
 	if(validationResult.error)
 		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
 
+	let userObj = await userDb.findOneWith({ _id: data.user.id })
+	if(!groupObj)
+		return { status: 403, code: "USER_DOES_NOT_EXIST" }
+	
 	let groupObj = await groupDb.findOneWith({ group_id: data.group_id })
 	if(!groupObj)
 		return { status: 403, code: "GROUP_DOES_NOT_EXIST" }
+
 
 	// Creating the new group based on the data of the old group
 	let newGroupResult = await createGroup({
@@ -38,7 +44,10 @@ async function newCycle(data){
 	//I will be creating a linked list, so as to keep track of the groups and all the groups that have been created from them
 	// So, I have to set the prev of the new group to the old group and the next group of the old group to the new group
 	newGroupObj = await groupDb.appendDoc({ group_id: newGroupObj.group_id }, "previous_group", groupObj.group_id)
-	groupObj = await groupDb.appendDoc({ group_id: groupObj }, "next_group", newGroupObj.group_id)
+	groupObj = await groupDb.appendDoc({ group_id: groupObj.group_id }, "next_group", newGroupObj.group_id)
+
+	console.log(groupObj)
+	console.log(newGroupObj)
 
 
 }
