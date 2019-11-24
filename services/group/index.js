b9bbf323232024f25834e6c8f3c380f1615ae961
iammadab <iammadab@gmail.com>
2019-11-24 11:26:29 +0100

    Add the add comment service to the group services

--- services/group/index.js
+++ services/group/index.js
@@ -1,11 +1,13 @@
 const createGroup = require("./group.create")
 const addMember = require("./group.addMember")
 const removeMember = require("./group.removeMember")
+const addComment = require("./group.addComment")
 
 const groupServices = {
 	createGroup,
 	addMember,
-	removeMember
+	removeMember,
+	addComment
 }
 
 module.exports = groupServices
