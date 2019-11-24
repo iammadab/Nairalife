a2d2654479f8a8aa07f6fe3b6b8e2cea302e7a16
iammadab <iammadab@gmail.com>
2019-11-24 11:21:31 +0100

    Using the create comment db function to create comments and sending the result

--- services/comment/comment.create.js
+++ services/comment/comment.create.js
@@ -2,10 +2,20 @@ const { createValidator } = require("lazy-validator")
 
 const createCommentValidator = createValidator("username.string, comment.string, user_id.number, group_id.number")
 
+const commentDb = require("../../data/db/comment.db")
+
 async function createComment(data){
 	let validationResult = createCommentValidator.parse(data)
 	if(validationResult.error)
 		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
+
+	let validData = validationResult.data
+
+	let commentObj = await commentDb.createComment(validData)
+	if(!commentObj)
+		return { status: 500, code: "PROBLEM_CREATING_COMMENT" }
+
+	return { status: 200, code: "COMMENT_CREATED", comment: commentObj._doc }
 }
 
 module.exports = createComment
