cf92562e9d400eb61d972f1aa00df713b5565807
iammadab <iammadab@gmail.com>
2019-11-24 21:08:18 +0100

    Fixed logic errors in the order function

--- services/group/group.start.js
+++ services/group/group.start.js
@@ -31,14 +31,14 @@ async function startGroup(data){
 	// if you return -1 a comes before b
 	// if you return 1 b comes before a
 	// if you return 0 they remain in their positions
-	async function orderFunction(a, b){
+	function orderFunction(a, b){
 		let userA = membersMap[a.user_id], userB = membersMap[b.user_id]
 
 		if(userA.nairascore > userB.nairascore) return -1
 		if(userA.nairascore > userB.nairascore) return 1
 		
 		let userAJoinDate = userA._id.getTimestamp(), userBJoinDate = userB._id.getTimestamp()
-		if(userAJoinDate > userBJoinDate) return -1
+		if(userAJoinDate < userBJoinDate) return -1
 		if(userAJoinDate > userBJoinDate) return 1
 
 		return 0
