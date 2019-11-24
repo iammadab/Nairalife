82ad5469091f72dc1d11f8bfee8e885f94ca3f28
iammadab <iammadab@gmail.com>
2019-11-24 12:57:15 +0100

    Added a sort option to the comments fetcher

--- services/page/functions/fetchComments.js
+++ services/page/functions/fetchComments.js
@@ -1,7 +1,10 @@
 const commentDb = require("../../../data/db/comment.db")
 
-async function fetchComments(querySpecifier){
-	return commentDb.findWith(querySpecifier)
+async function fetchComments(querySpecifier, options){
+	let cursor = commentDb.findWith(querySpecifier)
+	if(options.sort)
+		cursor = cursor.sort(options.sort)
+	return cursor
 }
 
 module.exports = fetchComments
