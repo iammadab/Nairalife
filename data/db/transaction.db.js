937d3d2a900413e75f2c9a038a1585736f783f99
iammadab <iammadab@gmail.com>
2019-11-25 18:09:45 +0100

    Fixed error in the transaction db

--- data/db/transaction.db.js
+++ data/db/transaction.db.js
@@ -2,7 +2,7 @@ const createDb = require("./base.db")
 const transactionModel = require("../models/transaction.model")
 const transactionDb = createDb(transactionModel)
 
-transactionDb.createTransactionm = function({ username, user_id, amount, reference, type, status, data }){
+transactionDb.createTransaction = function({ username, user_id, amount, reference, type, status, data }){
 	let newTransaction = new transactionModel({
 		username,
 		user_id,
