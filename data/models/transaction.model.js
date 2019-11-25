8b5c69281a76997b5f6fa68949d38abb89b8a865
iammadab <iammadab@gmail.com>
2019-11-25 17:46:42 +0100

    Created the transaction schema

--- data/models/transaction.model.js
+++ data/models/transaction.model.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose")
 
 const transactionSchema = mongoose.Schema({
-
+	username: { type: String, required: true },
+	user_id: { type: Number, required: true },
+	amount: { type: Number, required: true },
+	reference: { type: String, required: true },
+	status: { type: String, required: true, enum: ["success", "pending", "failed"] },
+	attempts: { type: Number },
+	data: { type: Object, default: {} }
 })
 
 const transactionModel = mongoose.model("Transaction", transactionSchema)
