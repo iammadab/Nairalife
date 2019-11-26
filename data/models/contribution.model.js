46d81231575ef9585276074c607b956f58a6898f
iammadab <iammadab@gmail.com>
2019-11-27 00:58:47 +0100

    Implemented the schema of the contribution

--- data/models/contribution.model.js
+++ data/models/contribution.model.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose")
 
 const contributionSchema = mongoose.Schema({
-
+	admin: { type: String, required: true },
+	admin_id: { type: Number, required: true },
+	group_id: { type: Number, required: true },
 })
 
 const contributionModel = mongoose.model("Contribution", contributionSchema)
