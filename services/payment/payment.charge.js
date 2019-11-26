df2b6bbb9e0e448f866a1aca56aa71886353efd2
iammadab <iammadab@gmail.com>
2019-11-26 08:19:24 +0100

    Applying the validation to the charge service

--- services/payment/payment.charge.js
+++ services/payment/payment.charge.js
@@ -1,5 +1,11 @@
-async function charge(data){
+const { createValidator } = require("lazy-validator")
 
+const chargeValidator = createValidator("user_id.number, amount.number")
+
+async function charge(data){
+	let validationResult = chargeValidator.parse(data)
+	if(validationResult.error)
+		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
 }
 
 module.exports = charge
