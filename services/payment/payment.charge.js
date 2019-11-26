0db1e27d041e2d07136586b7f4a1d9e6bfbccd6f
iammadab <iammadab@gmail.com>
2019-11-26 08:49:34 +0100

    Made sure that the user has a card before attempting charge

--- services/payment/payment.charge.js
+++ services/payment/payment.charge.js
@@ -1,8 +1,13 @@
+const axios = require("axios")
 const { createValidator } = require("lazy-validator")
 
 const chargeValidator = createValidator("user_id.number, amount.number")
 
 const userDb = require("../../data/db/user.db")
+
+const requestOptions = {
+	headers: { Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`}
+}
 
 async function charge(data){
 	let validationResult = chargeValidator.parse(data)
@@ -18,11 +23,35 @@ async function charge(data){
 	if(validData.amount < 1000)
 		return { status: 403, code: "MINIMUM_CHARGE_1000" }
 
+	if(!userObj.card)
+		return { status: 403, code: "NO_CARD_FOUND" }
+	console.log(userObj.card.authorization.authorization_code)
+
+	// let chargeResult = await charge({
+	// 	authorization_code
+	// })
+
 }
 
 module.exports = charge
 
 
-async function chargeUser(){
+async function chargeUser({ authorization_code, email, amount }){
+	let data = {
+		authorization_code,
+		email,
+		amount
+	}
 
+	return axios.post("https://api.paystack.co/transaction/charge_authorization", data, requestOptions)
+				.then(handleSuccess)
+				.catch(handleFailure)
+
+	function handleSuccess(response){
+		console.log(response)
+	}
+
+	function handleFailure(response){
+		console.log(response)
+	}
 }
