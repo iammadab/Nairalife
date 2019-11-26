58d355c47558768c3eb7457a8f707e4990378682
iammadab <iammadab@gmail.com>
2019-11-27 00:50:58 +0100

    Created basic structure for the contribution schema

---
+++ data/models/contribution.model.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose")
+
+const contributionSchema = mongoose.Schema({
+
+})
+
+const contributionModel = mongoose.model("Contribution", contributionSchema)
+
+module.exports = contributionModel
--- services/group/group.getContributions.js
+++ services/group/group.getContributions.js
@@ -41,8 +41,7 @@ async function getContributions(data){
 			reference: chargeResult.data.reference,
 			type: "contribution",
 			status: "pending"
-		})
-		
+		})		
 	}
 
 }
