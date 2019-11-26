d51adec1cfd13e6b0c48f7f5c06bd29bbc631659
iammadab <iammadab@gmail.com>
2019-11-27 00:42:36 +0100

    Charging each member and creating a transaction obj for them

--- services/group/group.getContributions.js
+++ services/group/group.getContributions.js
@@ -4,6 +4,7 @@ const getContributionsValidator = createValidator("group_id.number")
 
 const groupDb = require("../../data/db/group.db")
 const userDb = require("../../data/db/user.db")
+const transactionDb = require("../../data/db/transaction.db")
 
 const paymentService = require("../payment")
 
@@ -18,19 +19,30 @@ async function getContributions(data){
 	if(!groupObj)
 		return { status: 200, code: "GROUP_NOT_FOUND" }
 
+
+	// This is where I go through each group member
+	// Then pass their details to the payment charge service
 	for(let i = 0; i < groupObj.members.length; i++){
 		if(groupObj.members[i].removed) continue
 		let memberData = groupObj.members[i]
-		let userObj = await userDb.findOneWith({ user_id: memberData.user_id })
-		if(!userObj) continue
 
+		// Attempt charge
 		let chargeResult = await paymentService.charge({
-			authorization_code: userObj.card[0].authorization.authorization_code,
-			email: userObj.email,
+			user_id: memberData.user_id,
 			amount: groupObj.contribution_amount
 		})
 
-		console.log(chargeResult)
+		// The transaction object for the user
+		// The webhook will be the one to update the transactions based on the reference
+		let userTransaction = await transactionDb.createTransaction({
+			username: memberData.fullname,
+			user_id: memberData.user_id,
+			amount: groupObj.contribution_amount,
+			reference: chargeResult.data.reference,
+			type: "contribution",
+			status: "pending"
+		})
+		
 	}
 
 }
