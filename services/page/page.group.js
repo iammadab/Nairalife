67f04b075d3b00fb8320c2dd6b0ffd6369986aac
iammadab <iammadab@gmail.com>
2019-11-27 04:48:26 +0100

    Fetching the contributions in the page service for group

--- services/page/page.group.js
+++ services/page/page.group.js
@@ -2,7 +2,8 @@ const pageFunctions = require("./functions")
 
 async function group(req, res, next){
 	let group_id = req.params.group_id,
-		groupObj = await pageFunctions.fetchGroup(group_id)
+		groupObj = await pageFunctions.fetchGroup(group_id),
+		contributions = await pageFunctions.fetchContributions(group_id)
 
 	if(!groupObj)
 		return res.redirect("/admin/groups")
