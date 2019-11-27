b167c7e4e7ae9b3acaf49e9781cd4f89c972b2bb
iammadab <iammadab@gmail.com>
2019-11-27 17:40:05 +0100

    Making sure that the user exists

--- services/user/user.addBalance.js
+++ services/user/user.addBalance.js
@@ -2,10 +2,19 @@ const { createValidator } = require("lazy-validator")
 
 const addBalanceValidator = createValidator("user_id.number, amount.number")
 
+const userDb = require("../../data/db/user.db")
+
 async function addBalance(data){
 	let validationResult = addBalanceValidator.parse(data)
 	if(validationResult.error)
 		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
+
+	let validData = validationResult.data
+
+	let userObj = await userDb.findOneWith({ user_id: validData.user_id })
+	if(!userObj)
+		return { status: 403, code: "USER_NOT_FOUND" }
+
 }
 
 module.exports = addBalance
