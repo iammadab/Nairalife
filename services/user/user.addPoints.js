eca856a94116ccd00df5a1bb23a106ebc7a1c31a
iammadab <iammadab@gmail.com>
2019-11-27 16:58:26 +0100

    Added validation to the add points service

--- services/user/user.addPoints.js
+++ services/user/user.addPoints.js
@@ -1,5 +1,11 @@
-async function addPoints(data){
+const { createValidator } = require("lazy-validator")
 
+const addPointsValidator = createValidator("user_id.number, type.string, comment.string, points.number")
+
+async function addPoints(data){
+	let validationResult = addPointsValidator.parse(data)
+	if(validationResult.error)
+		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
 }
 
 module.exports = addPoints
