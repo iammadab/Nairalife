4b07ee963a266991a39773dbc6cc141bc321e06f
iammadab <iammadab@gmail.com>
2019-11-27 17:35:27 +0100

    Applied validation to add balance

--- services/user/user.addBalance.js
+++ services/user/user.addBalance.js
@@ -1,5 +1,11 @@
-async function addBalance(data){
+const { createValidator } = require("lazy-validator")
 
+const addBalanceValidator = createValidator("user_id.number, amount.number")
+
+async function addBalance(data){
+	let validationResult = addBalanceValidator.parse(data)
+	if(validationResult.error)
+		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
 }
 
 module.exports = addBalance
