6c246b8eb93c5952d671c8a46a689dcf09d0ce05
iammadab <iammadab@gmail.com>
2019-11-28 00:12:05 +0100

    Completed the point schema

--- data/models/point.model.js
+++ data/models/point.model.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose")
 
 const pointSchema = mongoose.Schema({
+	user_id: { type: Number, required: true },
+	type: { type: String, required: true },
+	comment: { type: String, required: true },
+	points: { type: Number, required: true },
 
+	admin_id: { type: Number, required: true },
+	admin: { type: String, required: true }
 })
 
 const pointModel = mongoose.model("Point", pointSchema)
