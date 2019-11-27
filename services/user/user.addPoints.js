09a1b6d2987979ac0ef1db9f7f1042c3faf5df81
iammadab <iammadab@gmail.com>
2019-11-27 17:15:02 +0100

    Updated the points user obj

--- services/user/user.addPoints.js
+++ services/user/user.addPoints.js
@@ -2,10 +2,25 @@ const { createValidator } = require("lazy-validator")
 
 const addPointsValidator = createValidator("user_id.number, type.string, comment.string, points.number")
 
+const userDb = require("../../data/db/user.db")
+
 async function addPoints(data){
 	let validationResult = addPointsValidator.parse(data)
 	if(validationResult.error)
 		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
+
+	let validData = validationResult.data
+
+	let userObj = await userDb.findOneWith({ user_id: validData.user_id })
+	if(!userObj)
+		return { status: 403, code: "USER_DOES_NOT_EXIST" }
+
+	let oldPoints = userobj.points, newPoints = oldPoints + validData.points
+	console.log(oldPoints, newPoints)
+	userObj = await userObj.appendDoc({ user_id: validData.user_id }, "points", newPoints)
+
+	console.log(userObj)
+
 }
 
 module.exports = addPoints
