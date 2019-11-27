53bec053b2eb672439eb87605a6f0c68e7ebb10e
iammadab <iammadab@gmail.com>
2019-11-27 17:36:41 +0100

    Verifying admin status for the balance route

--- routes/user.route.js
+++ routes/user.route.js
@@ -14,6 +14,6 @@ userRouter.post("/password", authenticationService.verifyToken(), bodyResponder(
 userRouter.post("/password/forgot", bodyResponder(userService.forgotPassword))
 userRouter.post("/withdraw", authenticationService.verifyToken(), bodyResponder(paymentService.withdraw))
 userRouter.post("/points", authenticationService.verifyToken(), authenticationService.validateAdmin, bodyResponder(userService.addPoints))
-userRouter.post("/balance", bodyResponder(userService.addBalance))
+userRouter.post("/balance", authenticationService.verifyToken(), authenticationService.validateAdmin, bodyResponder(userService.addBalance))
 
 module.exports = userRouter
