54470eb797be1a5b35c3dc2be6bc3b77aa5e7169
iammadab <iammadab@gmail.com>
2019-11-27 04:50:09 +0100

    Edited the fetch contributions function to require the contribution id

--- services/page/functions/fetchContributions.js
+++ services/page/functions/fetchContributions.js
@@ -1,7 +1,7 @@
 const contributionDb = require("../../../data/db/contribution.db")
 
-async function fetchContributions(querySpecifier){
-	return contributionDb.findWith(querySpecifier)
+async function fetchContributions(group_id){
+	return contributionDb.findWith({ group_id })
 }
 
 module.exports = fetchContributions
