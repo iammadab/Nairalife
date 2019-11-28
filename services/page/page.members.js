958452f17b50699eb9c3535df06521ed9a280ebf
iammadab <iammadab@gmail.com>
2019-11-28 15:26:58 +0100

    Fetching all members in the members page service

--- services/page/page.members.js
+++ services/page/page.members.js
@@ -1,5 +1,15 @@
-async function members(req, res, next){
+const pageFunctions = require("./functions")
 
+async function members(req, res, next){
+	let allMembers = await pageFunctions.fetchUsers()
+
+	console.log(allMembers)
+
+	req.body.pageData = {
+		members: allMembers
+	}
+
+	next()
 }
 
 module.exports = members
