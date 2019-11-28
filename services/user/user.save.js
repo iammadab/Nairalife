bfe6f847b32c283db160d06fbd81e7716aa132e2
iammadab <iammadab@gmail.com>
2019-11-28 18:42:53 +0100

    Applying validation to the user save service

--- services/user/user.save.js
+++ services/user/user.save.js
@@ -1,5 +1,11 @@
-async function save(data){
+const { createValidator } = require("lazy-validator")
 
+const saveValidator = createValidator("user_id.number")
+
+async function save(data){
+	let validationResult = saveValidator.parse(data)
+	if(validationResult.error)
+		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
 }
 
 module.exports = save
