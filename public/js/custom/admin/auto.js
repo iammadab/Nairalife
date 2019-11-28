1242ff5a648e1fcdd5f0e517d317e8bf516b2d92
iammadab <iammadab@gmail.com>
2019-11-28 19:41:30 +0100

    Proper button states change

--- public/js/custom/admin/auto.js
+++ public/js/custom/admin/auto.js
@@ -7,11 +7,11 @@ let store = {
 	addEvent(store.saveButtons, "click", autoSave)
 })()
 
-let saveButton = createButton(".save-button-text", "Save", "Saving...")
 function autoSave(event){
 	event.preventDefault()
-	saveButton()
 	let user_id = event.target.dataset.user_id
+	let saveButton = createButton(`.save-button-text-${user_id}`, "Save", "Saving...")
+	saveButton()
 
 	return api("user/save", { user_id: user_id, token: getToken("atoken") })
 			.then(handleResponse)
