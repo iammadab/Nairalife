c6089c3fb2c49361bb4375ea5f648ea660df8033
iammadab <iammadab@gmail.com>
2019-11-28 18:25:17 +0100

    Done with auto save listing

--- services/page/page.auto.js
+++ services/page/page.auto.js
@@ -2,7 +2,6 @@ const pageFunctions = require("./functions")
 
 async function auto(req, res, next){
 	let autoSaveMembers = await pageFunctions.fetchUsers({ status: "autosave" })
-	console.log(autoSaveMembers)
 
 	req.body.pageData = {
 		autoSaveMembers
