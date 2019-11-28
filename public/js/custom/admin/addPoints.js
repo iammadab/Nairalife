8127cae40f81ba0b76c39b90c40ffb115e9b6236
iammadab <iammadab@gmail.com>
2019-11-28 13:33:10 +0100

    Added events to stop showing the alerts on input focus

--- public/js/custom/admin/addPoints.js
+++ public/js/custom/admin/addPoints.js
@@ -3,3 +3,14 @@ let store = {
 	addPointsFormTag: ".add-points-form",
 	addPointsInputs: document.querySelectorAll(".add-points-form input, .add-points-form select, .add-points-form textarea")
 }
+
+;(function attachEvents(){
+	addEvent([store.addPointsButton], "click", addPoints)
+	addEvent(addPointsInputs, "input,focus", () => hideAlert("add-points-error"))
+	addEvent(addPointsInputs, "input,focus", () => hideAlert("add-points-success"))
+})()
+
+function addPoints(event){
+	event.preventDefault()
+	console.log("Adding the points")
+}
