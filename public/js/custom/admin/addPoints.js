f1fbdee88327f479cd3395eb33d506633ab3609c
iammadab <iammadab@gmail.com>
2019-11-28 13:30:20 +0100

    Fetched all the appropriate elements in the store

---
+++ public/js/custom/admin/addPoints.js
@@ -0,0 +1,5 @@
+let store = {
+	addPointsButton: document.querySelector(".add-points-button"),
+	addPointsFormTag: ".add-points-form",
+	addPointsInputs: document.querySelectorAll(".add-points-form input, .add-points-form select, .add-points-form textarea")
+}
