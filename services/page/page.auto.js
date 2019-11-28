a1c178c0dce9aac028ddcfe6185da4ded042a7b0
iammadab <iammadab@gmail.com>
2019-11-28 18:12:40 +0100

    Fetching all autosave members in the auto page

--- services/page/page.auto.js
+++ services/page/page.auto.js
@@ -1,5 +1,14 @@
-async function auto(req, res, next){
+const pageFunctions = require("./functions")
 
+async function auto(req, res, next){
+	let autoSaveMembers = await pageFunctions.fetchUsers({ status: "autosave" })
+	console.log(autoSaveMembers)
+
+	req.body.pageData = {
+		autoSaveMembers
+	}
+
+	next()
 }
 
 module.exports = auto
