fe717e8b9a14170fd0e2d4aa02c63ea9ea9b9398
iammadab <iammadab@gmail.com>
2019-11-28 02:33:43 +0100

    Converting the string to object id to fetch transactions

--- services/page/functions/fetchContributionStatus.js
+++ services/page/functions/fetchContributionStatus.js
@@ -1,9 +1,14 @@
+const mongoose = require("mongoose")
+const ObjectId = mongoose.Types.ObjectId
+
 const contributionDb = require("../../../data/db/contribution.db")
 const transactionDb = require("../../../data/db/transaction.db")
 
 async function getContributionStatus(contribution_id){
-	let transactions = await transactionDb.findWith({ "data.contribution_id": contribution_id })
+	let transactions = await transactionDb.findWith({ "data.contribution_id": ObjectId(contribution_id) })
 	console.log(transactions)
 }
 
 module.exports = getContributionStatus
+
+getContributionStatus("5dddc04639a7aa1af06ea06e").then(console.log)
