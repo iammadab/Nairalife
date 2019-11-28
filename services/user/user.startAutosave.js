b724ae652f65d2ae2cfb710caf3f0038902dbcb0
iammadab <iammadab@gmail.com>
2019-11-28 23:14:05 +0100

    Adding the autosave start date

--- services/user/user.startAutosave.js
+++ services/user/user.startAutosave.js
@@ -9,6 +9,11 @@ async function startAutosave(data){
 	if(!userObj)
 		return { status: 403, code: "PROBLEM_SETTING_STATUS" }
 
+	// Set the autosave start date
+	userObj = await userDb.appendDoc({ _id: data.user.id }, "autosave_start", new Date())
+	if(!userObj)
+		return { status: 403, code: "PROBLEM_SETTING_DATE" }
+
 	userObj = await userDb.appendDoc({ _id: data.user.id }, "stage", "active")
 	if(!userObj)
 		return { status: 403, code: "PROBLEM_SETTING_STAGE" }
