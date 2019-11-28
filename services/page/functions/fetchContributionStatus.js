98248f7eafdaade1c9f4bfed2d4f0c9d3f2b6d0e
iammadab <iammadab@gmail.com>
2019-11-28 02:40:01 +0100

    Implemented function to get the default rate and total contributions

--- services/page/functions/fetchContributionStatus.js
+++ services/page/functions/fetchContributionStatus.js
@@ -5,8 +5,20 @@ const contributionDb = require("../../../data/db/contribution.db")
 const transactionDb = require("../../../data/db/transaction.db")
 
 async function getContributionStatus(contribution_id){
-	let transactions = await transactionDb.findWith({ "data.contribution_id": ObjectId(contribution_id) })
-	console.log(transactions)
+	let allTransactions = await transactionDb.findWith({ "data.contribution_id": ObjectId(contribution_id) })
+	let successfullTransactions = await transactionDb.findWith({ status: "success", "data.contribution_id": ObjectId(contribution_id) })
+	
+	let defaultRate = ((allTransactions.length - successfullTransactions.length) / allTransactions.length) * 100
+	let totalContribution = 0
+
+	for(let i = 0; i < successfullTransactions.length; i++){
+		totalContribution += successfullTransactions.amount
+	}
+
+	return {
+		defaultRate,
+		totalContribution
+	}
 }
 
 module.exports = getContributionStatus
