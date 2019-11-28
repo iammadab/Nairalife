951de58fd44bf82bf52c074e22d569d6e7e5e327
iammadab <iammadab@gmail.com>
2019-11-28 16:17:50 +0100

    Changing the status and the stage on user autosave

--- services/user/user.startAutosave.js
+++ services/user/user.startAutosave.js
@@ -1,4 +1,19 @@
+const userDb = require("../../data/db/user.db")
+
 async function startAutosave(data){
+	let userObj = await userDb.findOneWith({ _id: data.user.id })
+	if(!userObj)
+		return { status: 403, code: "USER_NOT_FOUND" }
+
+	userObj = await userDb.appendDoc({ _id: data.user.id }, "status", "autosave")
+	if(!userObj)
+		return { status: 403, code: "PROBLEM_SETTING_STATUS" }
+
+	userObj = await userDb.appendDoc({ _id: data.user.id }, "stage", "active")
+	if(!userObj)
+		return { status: 403, code: "PROBLEM_SETTING_STAGE" }
+
+	
 
 }
 
