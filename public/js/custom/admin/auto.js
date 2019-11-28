e57df58117db0f799686a6d2748e4b11c501f1c2
iammadab <iammadab@gmail.com>
2019-11-28 19:23:29 +0100

    On save click, communicate with the api

--- public/js/custom/admin/auto.js
+++ public/js/custom/admin/auto.js
@@ -1,3 +1,19 @@
 let store = {
 	saveButtons: document.querySelectorAll(".save-button")
 }
+
+;(function attachEvents(){
+	addEvent(store.saveButtons, "click", autoSave)
+})()
+
+function autoSave(event){
+	event.preventDefault()
+	let user_id = event.target.dataset.user_id
+
+	return api("user/save", { user_id: user_id, token: getToken("atoken") })
+			.then(handleResponse)
+
+	function handleResponse(response){
+		console.log(response)
+	}
+}
