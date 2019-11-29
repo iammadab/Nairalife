c5deaa850c5dc14a2c4153013b8977b75ab30a9e
iammadab <iammadab@gmail.com>
2019-11-29 11:50:03 +0100

    Adding the hypen date to the points

--- services/page/page.points.js
+++ services/page/page.points.js
@@ -1,7 +1,11 @@
 const pageFunctions = require("./functions")
 
 async function points(req, res, next){
-	let allPoints = await pageFunctions.fetchPoints({ user_id: req.body.user.id })
+	let allPoints = await pageFunctions.fetchPoints({ user_id: req.body.user.user_id })
+	
+	allPoints.forEach(pointDoc => {
+		pointDoc._doc.created_at = pageFunctions.createDate(pointDoc._id.getTimestamp()).getHypenDate()
+	})
 
 	req.body.pageData = {
 		allPoints
