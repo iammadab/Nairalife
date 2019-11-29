c6a4273fdfa7765e32084b0094ff9aa7e18c8a64
iammadab <iammadab@gmail.com>
2019-11-29 13:17:32 +0100

    Attaching the date and time to the notification object

--- services/page/page.notifications.js
+++ services/page/page.notifications.js
@@ -3,7 +3,10 @@ const pageFunctions = require("./functions")
 async function notifications(req, res, next){
 	let userNotifications = await pageFunctions.fetchNotifications({ user_id: req.body.user.user_id })
 
-	console.log(userNotifications)
+	userNotifications.forEach(notification => {
+		let notificationDate = pageFunctions.createDate(notification.created_at)
+		notification._doc.created_at = [notificationDate.getDate(), notificationDate.getTime()]
+	})
 
 	req.body.pageData = {
 		userNotifications
