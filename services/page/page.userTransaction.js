52b56529b49322eabb33d6d7f1ac142bcffd044c
iammadab <iammadab@gmail.com>
2019-11-29 12:20:52 +0100

    Fetching all the transactions for a user and attaching it to the req

--- services/page/page.userTransaction.js
+++ services/page/page.userTransaction.js
@@ -1,7 +1,15 @@
 const pageFunctions = require("./functions")
 
 async function userTransaction(req, res, next){
+	let allTransactions = await pageFunctions.fetchTransactions({ user_id: req.body.user.user_id })
 
+	console.log(allTransactions)
+
+	req.body.pageData = {
+		allTransactions
+	}
+
+	next()
 }
 
 module.exports = userTransaction
