27a43d4fa3df29ea2362f5b66697eb08a94504fe
iammadab <iammadab@gmail.com>
2019-11-29 13:42:00 +0100

    Resetting the user notification count

--- services/page/page.notifications.js
+++ services/page/page.notifications.js
@@ -1,7 +1,11 @@
 const pageFunctions = require("./functions")
+const userDb = require("../../data/db/user.db")
 
 async function notifications(req, res, next){
 	let userNotifications = await pageFunctions.fetchNotifications({ user_id: req.body.user.user_id })
+
+	// Reset the user notification count to 0
+	await userDb.appendDoc({ user_id: req.body.user.user_id }, "notification_count", 0)
 
 	userNotifications.forEach(notification => {
 		let notificationDate = pageFunctions.createDate(notification.created_at)
