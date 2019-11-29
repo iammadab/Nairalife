3ec44d0fcbda52d739e84d6f15a6fc54da0cb726
iammadab <iammadab@gmail.com>
2019-11-29 14:08:00 +0100

    Hiding the request error on store input

--- public/js/custom/higher.js
+++ public/js/custom/higher.js
@@ -6,8 +6,10 @@ let store = {
 
 ;(function attachEvents(){
 	addEvent([store.requestButton], "click", requestCar)
+	addEvent(store.requestInputs, "input,focus", hideAlert("request-error"))
 })()
 
 function requestCar(event){
 	event.preventDefault()
+	showAlert("request-error", "You can't request for a car because your nairascore is not up to 70")
 }
