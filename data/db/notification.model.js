948837f3a7a1557141f57b806d67dbbced70d28b
iammadab <iammadab@gmail.com>
2019-11-29 12:58:51 +0100

    Created the notification create function in the db

--- data/db/notification.model.js
+++ data/db/notification.model.js
@@ -2,4 +2,13 @@ const createDb = require("./base.db")
 const notificationModel = require("../models/notification.model")
 const notificationDb = createDb(notificationModel)
 
+notificationDb.createNotification = function({ user_id, notification }){
+	let newNotification = new notificationModel({
+		user_id,
+		notification
+	})
+
+	return newNotification.save()
+}
+
 module.exports = notificationDb
