0d52a377039567fdfbafb299a75643a844154f1f
iammadab <iammadab@gmail.com>
2019-11-29 07:10:51 +0100

    Getting the transaction made for each autsave member from the start of the day

--- services/page/page.auto.js
+++ services/page/page.auto.js
@@ -3,18 +3,16 @@ const pageFunctions = require("./functions")
 async function auto(req, res, next){
 	let autoSaveMembers = await pageFunctions.fetchUsers({ status: "autosave" })
 	let midnight = new Date((new Date()).setHours(0, 0, 0, 0))
-	// console.log(midnight)
 
 	for(let i = 0; i < autoSaveMembers.length; i++){
 		let baseData = {
 			user_id: autoSaveMembers[i].user_id,
 			type: "autosave"
 		}
-		console.log(baseData)
 
-		let successfulTransactions = await pageFunctions.fetchTransactions({ ...baseData, status: "success", created_at: { $gt: midnight }})
-		let pendingTransactions = await pageFunctions.fetchTransactions({ ...baseData, status: "pending", created_at: { $gt: midnight }})
-		let failedTransactions = await pageFunctions.fetchTransactions({ ...baseData, status: "failed", created_at: { $gt: midnight }})
+		let successfulTransactions = await pageFunctions.fetchTransactions({ ...baseData, status: "success", created_at: { $gte: midnight }})
+		let pendingTransactions = await pageFunctions.fetchTransactions({ ...baseData, status: "pending", created_at: { $gte: midnight }})
+		let failedTransactions = await pageFunctions.fetchTransactions({ ...baseData, status: "failed", created_at: { $gte: midnight }})
 
 		console.log(successfulTransactions)
 		console.log("")
