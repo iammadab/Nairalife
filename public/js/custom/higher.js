51341d17539609566fb352e529bc4e62c538814b
iammadab <iammadab@gmail.com>
2019-11-29 14:04:44 +0100

    Attach event to the request car button

--- public/js/custom/higher.js
+++ public/js/custom/higher.js
@@ -1,3 +1,13 @@
 let store = {
-	
+	requestButton: document.querySelector(".request-button"),
+	requestInputs: document.querySelectorAll(".request-form input"),
+	requestFormTag: ".request-form"
+}
+
+;(function attachEvents(){
+	addEvent([store.requestButton], "click", requestCar)
+})()
+
+function requestCar(event){
+	event.preventDefault()
 }
