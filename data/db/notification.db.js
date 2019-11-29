11a21932caf25934bb081589d10a5a7ad37e8511
iammadab <iammadab@gmail.com>
2019-11-29 13:02:20 +0100

    Importing the user db in the notification db

--- data/db/notification.db.js
+++ data/db/notification.db.js
@@ -2,6 +2,8 @@ const createDb = require("./base.db")
 const notificationModel = require("../models/notification.model")
 const notificationDb = createDb(notificationModel)
 
+const userDb = require("./user.db")
+
 notificationDb.createNotification = function({ user_id, notification }){
 	let newNotification = new notificationModel({
 		user_id,
