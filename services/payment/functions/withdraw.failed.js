3195548fafe77fda45725eb0ac9e343947e95dd5
iammadab <iammadab@gmail.com>
2019-11-30 11:29:05 +0100

    Implemented transaction failed

--- services/payment/functions/withdraw.failed.js
+++ services/payment/functions/withdraw.failed.js
@@ -3,6 +3,14 @@ const transactionDb = require("../../../data/db/transaction.db")
 async function withdraw_failed(data){
 	console.log("Withdraw failure")
 	console.log(data)
+
+	let transactionObj = await transactionDb.findOneWith({ reference: data.reference })
+	if(!transactionObj)
+		return console.log("Couldn't find the transaction obj for ", data.reference)
+
+	// Set the status of the transaction to failed
+	transactionDb.appendDoc({ reference: data.reference }, "status", "failed")
+	
 }
 
 module.exports = withdraw_failed
