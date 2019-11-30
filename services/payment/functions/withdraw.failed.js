8235b6f3f86f25669bacee93fb14421bb459a6f7
iammadab <iammadab@gmail.com>
2019-11-30 11:35:13 +0100

    Added proper logging to the withdraw

--- services/payment/functions/withdraw.failed.js
+++ services/payment/functions/withdraw.failed.js
@@ -2,15 +2,17 @@ const transactionDb = require("../../../data/db/transaction.db")
 
 async function withdraw_failed(data){
 	console.log("Withdraw failure")
+	console.log("----------------")
 	console.log(data)
 
 	let transactionObj = await transactionDb.findOneWith({ reference: data.reference })
 	if(!transactionObj)
 		return console.log("Couldn't find the transaction obj for ", data.reference)
+	console.log(transactionObj)
 
 	// Set the status of the transaction to failed
-	transactionDb.appendDoc({ reference: data.reference }, "status", "failed")
-	
+	transactionObj = transactionDb.appendDoc({ reference: data.reference }, "status", "failed")
+	console.log(transactionObj)
 }
 
 module.exports = withdraw_failed
--- services/payment/functions/withdraw.success.js
+++ services/payment/functions/withdraw.success.js
@@ -1,6 +1,11 @@
+const transactionDb = require("../../../data/db/transaction.db")
+const userDb = require("../../../data/db/user.db")
+
 async function withdraw_success(data){
 	console.log("Withdraw success")
-	console.log(data)
+	console.log(data)	
+
+	
 }
 
 module.exports = withdraw_success
