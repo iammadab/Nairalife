e32a94aa4f7f1c49c3a12f6d04249bbdddfa9d87
iammadab <iammadab@gmail.com>
2019-11-30 11:04:47 +0100

    Routing the transaction events to the webhook

--- routes/webhook.route.js
+++ routes/webhook.route.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const webhookRouter = express.Router()
 
+const paymentFunctions = require("../services/payment/functions")
+console.log(paymentFunctions)
+
 webhookRouter.post("/paystack", function(req, res){
 	console.log("Paystack web hook")
 	console.log(req.body)
@@ -9,10 +12,17 @@ webhookRouter.post("/paystack", function(req, res){
 		return console.log("Invalid paystack request body")
 	}
 
-	// switch(req.body.event){
-	// 	case "transaction.success":
-		
-	// }
+	switch(req.body.event){
+		case "transaction.success":
+			paymentFunctions.withdrawSuccess(req.body.data)
+			break
+		case "transaction.failed":
+			paymentFunctions.withdrawFailed(req.body.data)
+			break
+		case "charge.success":
+			paymentFunctions.chargeSuccess(req.body.data)		
+			break
+	}
 
 })
 
