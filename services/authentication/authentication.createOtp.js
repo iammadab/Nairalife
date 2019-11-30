d28718e2cb5b517ec223e31a44ebecb919caaccb
iammadab <iammadab@gmail.com>
2019-11-30 17:08:53 +0100

    Fixed otp problem

--- services/authentication/authentication.createOtp.js
+++ services/authentication/authentication.createOtp.js
@@ -1,5 +1,5 @@
 const { createValidator } = require("lazy-validator")
-const { sendMessage } = require("../../lib/whatsapp")
+const sendMessage = require("../../lib/whatsapp")
 
 const otpDb = require("../../data/db/otp.db")
 const userDb = require("../../data/db/user.db")
