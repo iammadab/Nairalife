9e7d6a855b8342f43221226dda56321e608ebffa
iammadab <iammadab@gmail.com>
2019-11-30 11:02:24 +0100

    Added the payments function to one object

--- services/payment/functions/index.js
+++ services/payment/functions/index.js
@@ -1,5 +1,11 @@
-const paymentFunctions = {
+const withdrawSuccess = require("./withdraw.success")
+const withdrawFailed = require("./withdraw.failed")
+const chargeSuccess = require("./charge.success")
 
+const paymentFunctions = {
+	withdrawSuccess,
+	withdrawFailed,
+	chargeSuccess
 }
 
 module.exports = paymentFunctions
