8f5b2551815d24a8f78a97a987a70a14388f2525
iammadab <iammadab@gmail.com>
2019-11-30 10:58:38 +0100

    Created the payments functions

--- routes/webhook.route.js
+++ routes/webhook.route.js
@@ -4,6 +4,16 @@ const webhookRouter = express.Router()
 webhookRouter.post("/paystack", function(req, res){
 	console.log("Paystack web hook")
 	console.log(req.body)
+
+	if(!req.body.event){
+		return console.log("Invalid paystack request body")
+	}
+
+	// switch(req.body.event){
+	// 	case "transaction.success":
+		
+	// }
+
 })
 
 module.exports = webhookRouter
---
+++ services/payment/functions/index.js
@@ -0,0 +1,5 @@
+const paymentFunctions = {
+
+}
+
+module.exports = paymentFunctions
