0421819cc9682652c7e070427d65e778bbdd5fbc
iammadab <iammadab@gmail.com>
2019-12-06 22:16:00 +0100

    Created the message map

--- public/js/custom/plan.js
+++ public/js/custom/plan.js
@@ -11,3 +11,12 @@ function updateState(event){
 	event.preventDefault()
 	store.planState[event.target.name] = event.target.value ? event.target.value : null
 }
+
+function updateStatus(){
+	let messageMap = {
+		"2000000,daily": "You are doing the 2m daily",
+		"2000000,weekly": "You are doing the 2m weekly",
+		"2500000,daily": "You are doing the 2.5m daily",
+		"2500000,weekly": "You are doing the 2.5m weekly"
+	}
+}
