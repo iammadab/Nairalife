9fb164858873320f11a01bbea5412f81f410f25d
iammadab <iammadab@gmail.com>
2019-12-06 23:34:12 +0100

    Applying validation to the user plan

--- services/user/user.plan.js
+++ services/user/user.plan.js
@@ -1,5 +1,11 @@
-async function userPlan(data){
+const { createValidator } = require("lazy-validator")
 
+const userPlanValidator = createValidator("total_amount.number, period.string, amount.number")
+
+async function userPlan(data){
+	let validationResult = userPlanValidator.parse(data)
+	if(validationResult.error)
+		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
 }
 
 module.exports = userPlan
