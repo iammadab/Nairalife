e897382e248570a7d45ad3c7120e9ae7e43bea5d
iammadab <iammadab@gmail.com>
2019-12-06 23:13:26 +0100

    Replaced amount with total amount

--- public/js/custom/plan.js
+++ public/js/custom/plan.js
@@ -1,13 +1,13 @@
 let store = {
-	planState: { amount: null, period: null },
+	planState: { totalAmount: null, period: null },
 	planInputs: document.querySelectorAll("select"),
 	choosePlanButton: document.querySelector(".choose-plan-button")
 }
 
 let messageMap = {
-	"2000000,daily": [ "You are doing the 2m daily", 6000 ]
-	"2000000,weekly": [ "You are doing the 2m weekly", 40000 ]
-	"2500000,daily": [ "You are doing the 2.5m daily", 4000 ]
+	"2000000,daily": [ "You are doing the 2m daily", 6000 ],
+	"2000000,weekly": [ "You are doing the 2m weekly", 40000 ],
+	"2500000,daily": [ "You are doing the 2.5m daily", 4000 ],
 	"2500000,weekly": [ "You are doing the 2.5m weekly", 30000 ]
 }
 
@@ -20,11 +20,11 @@ let messageMap = {
 function updateState(event){
 	event.preventDefault()
 	store.planState[event.target.name] = event.target.value ? event.target.value : null
-	updateStatus(`${store.planState.amount},${store.planState.period}`)
+	updateStatus(`${store.planState.totalAmount},${store.planState.period}`)
 }
 
 function updateStatus(currentStatus){
-	let message = messageMap[currentStatus.toLowerCase()]
+	let messageArray = messageMap[currentStatus.toLowerCase()], message = messageArray ? messageArray[0] : ""
 	if(!message)
 		message = "Choose a plan above"
 
@@ -33,6 +33,9 @@ function updateStatus(currentStatus){
 
 function addPlan(event){
 	event.preventDefault()
-	if(!store.planState.amount || !store.planState.period)
+	if(!store.planState.totalAmount || !store.planState.period)
 		showAlert("plan-error", "Choose a plan below")
+
+	let data = { ...planState }
+	console.log(data)
 }
