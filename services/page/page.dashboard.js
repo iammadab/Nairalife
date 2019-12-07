d521d5933109acbf3a8972d075eecfaa64f71826
iammadab <iammadab@gmail.com>
2019-12-07 17:52:49 +0100

    Passing the higher purchase transactions to the dashboard

--- services/page/page.dashboard.js
+++ services/page/page.dashboard.js
@@ -3,35 +3,15 @@ const userDb = require("../../data/db/user.db")
 
 async function dashboard(req, res, next){
 	let userObj = await pageFunctions.fetchUser(req.body.user.id)
-	let groupObj = await pageFunctions.fetchGroup(userObj.group)
-	let autoSaveTransactions = await pageFunctions.fetchTransactions({ user_id: userObj.user_id, type: "autosave" })
-	let members = [], comments = []
+	let higherPurchaseTransactions = await pageFunctions.fetchTransactions({ user_id: userObj.user_id, type: "higher_purchase" })
 
-	if(groupObj){
-		for(let i = 0; i < groupObj.members.length; i++){
-			let member = groupObj.members[i]
-			if(member.removed) continue
-			let memberObj = await userDb.findOneWith({ user_id: member.user_id })
-			members.push({ ...memberObj._doc, join_date: member.join_date })
-		}
-	}
-
-	if(groupObj){
-		comments = await pageFunctions.fetchComments({ group_id: groupObj.group_id }, { sort: { created_at: -1 }})
-	}
-
-	autoSaveTransactions.forEach(transaction => {
+	higherPurchaseTransactions.forEach(transaction => {
 		transaction._doc.created_at = pageFunctions.createDate(transaction._id.getTimestamp()).getHypenDate()
 	})
 	
-	// console.log(members)
-	// console.log(comments)
 	req.body.pageData = {
 		user: userObj,
-		group: groupObj,
-		members,
-		comments,
-		autoSaveTransactions
+		higherPurchaseTransactions
 	}
 
 	next()
