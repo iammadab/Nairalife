c8ec3ce83d81d6388313729242b2f1c1764f77dd
iammadab <iammadab@gmail.com>
2019-12-07 19:08:06 +0100

    Fixed it from the top

--- services/page/page.profile.js
+++ services/page/page.profile.js
@@ -2,10 +2,25 @@ const pageFunctions = require("./functions")
 const userDb = require("../../data/db/user.db")
 
 async function profile(req, res, next){
-	let userObj = await userDb.findOneWith({ user_id: req.params.user_id })
+	let userObj = (await userDb.findOneWith({ user_id: req.params.user_id }))._doc
+	let higherPurchaseTransactions = await pageFunctions.fetchTransactions({ user_id: userObj.user_id, type: "higher_purchase" })
+	let totalPayment = 0, remainingPayment = 0
+
+	if(userObj)
+		userObj.created_at = pageFunctions.createDate(userObj._id.getTimestamp()).getDate()
+
+	higherPurchaseTransactions.forEach(transaction => {
+		transaction._doc.created_at = pageFunctions.createDate(transaction._id.getTimestamp()).getHypenDate()
+		totalPayment += Number(transaction.amount)
+	})
+
+	remainingPayment = Number(userObj.plan.total_amount) - totalPayment
 
 	req.body.pageData = {
-		user: userObj._doc
+		user: userObj,
+		higherPurchaseTransactions,
+		totalPayment,
+		remainingPayment
 	}
 
 	next()
