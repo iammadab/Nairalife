432140480a7f42a6734d77eba79fa582a019e653
iammadab <iammadab@gmail.com>
2019-12-07 18:57:17 +0100

    Passing the user data to the profile page

--- services/page/page.profile.js
+++ services/page/page.profile.js
@@ -3,7 +3,12 @@ const userDb = require("../../data/db/user.db")
 
 async function profile(req, res, next){
 	let userObj = await userDb.findOneWith({ user_id: req.params.user_id })
-	console.log(userObj)
+
+	req.body.pageData = {
+		user: userObj
+	}
+
+	next()
 }
 
 module.exports = profile
