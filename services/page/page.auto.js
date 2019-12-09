6527e7ba176b46b63a54d21c87cc81a01a711136
iammadab <iammadab@gmail.com>
2019-12-09 09:29:30 +0100

    Enhanced the autosave section

--- services/page/page.auto.js
+++ services/page/page.auto.js
@@ -23,7 +23,49 @@ async function auto(req, res, next){
 
 		member._doc.totalPayment = totalPayment
 		member._doc.remainingPayment = member.plan.total_amount - totalPayment
-		member._doc.phase = "failed"
+
+		// Now that rendering data has been collected, we need to determine if the user can save today
+		// It's based on two factors, the payment plan chosen and the transactions that have already happened
+		// Starting with the payment plan
+		// First step is to get the plan the member is currently on
+		let planObj = member.status == "payment_one" ? member.payment_one : member.plan
+
+		if(planObj.start_date){
+			// Each user has a date that they started payment
+			// And they have a period that they have chosen to pay
+			// We need to check if today falls in that period
+			// First we set an even playing field by setting today to midnight and setting the day they started to midnight 
+			let midnight = new Date((new Date()).setHours(0, 0, 0, 0))
+			let paymentStartMidnight = (new Date(planObj.start_date)).setHours(0, 0, 0, 0)
+			
+			// Next we get the amount of milliseconds that have passed since they started paying till today
+			let datePassed = Number(midnight) - Number(paymentStartMidnight)
+			console.log("Date passed", datePassed)
+
+			// We convert that to a date and then extract the date
+			// Since the new date will be at midnight, the date shifts by one
+			// We fix this by subtracting one after getting the date
+			let days = (new Date(datePassed)).getDate() - 1
+
+			// This maps the payment period to the days
+			let periodMap = {
+				daily: 1,
+				weekly: 7,
+				monthly: 30
+			}
+
+			// Here we get the contribution period value
+			// Then check if the days passed fall under that period
+			// If it does we continue to check transactions
+			// If it doesn't we set the phase to success and continue as nothing should be done
+			let periodCount = periodMap[planObj.period.toLowerCase()]
+			console.log("Period count", periodCount)
+			if(periodCount && days % periodCount != 0){
+				member._doc.phase = "success"
+				continue
+			}
+		}
+		// member._doc.phase = "noattempt"
 	}
 
 	req.body.pageData = {
@@ -39,38 +81,38 @@ async function auto(req, res, next){
 
 // 	for(let i = 0; i < autoSaveMembers.length; i++){
 // 		// Check if the user can save today
-// 		if(autoSaveMembers[i].autosave_start){
-// 			// Each user has a date that they started autosave
-// 			// And they have a period that they have chosen to save
-// 			// We need to check if today falls in that period
-// 			// First we set an even playing field by setting today to midnight and setting the day they started to midnight 
-// 			let autoSaveMidnight = (new Date(autoSaveMembers[i].autosave_start)).setHours(0, 0, 0, 0)
+		// if(autoSaveMembers[i].autosave_start){
+		// 	// Each user has a date that they started autosave
+		// 	// And they have a period that they have chosen to save
+		// 	// We need to check if today falls in that period
+		// 	// First we set an even playing field by setting today to midnight and setting the day they started to midnight 
+		// 	let autoSaveMidnight = (new Date(autoSaveMembers[i].autosave_start)).setHours(0, 0, 0, 0)
 			
-// 			// Next we get the amount of milliseconds that have passed since they started saving till today
-// 			let datePassed = Number(midnight) - Number(autoSaveMidnight)
+		// 	// Next we get the amount of milliseconds that have passed since they started saving till today
+		// 	let datePassed = Number(midnight) - Number(autoSaveMidnight)
 
-// 			// We convert that to a date and then extract the date
-// 			// Since the new date will be at midnight, the date shifts by one
-// 			// We fix this by subtracting one after getting the date
-// 			let days = (new Date(datePassed)).getDate() - 1
+		// 	// We convert that to a date and then extract the date
+		// 	// Since the new date will be at midnight, the date shifts by one
+		// 	// We fix this by subtracting one after getting the date
+		// 	let days = (new Date(datePassed)).getDate() - 1
 
-// 			// This maps the contribution period to the days
-// 			let periodMap = {
-// 				daily: 1,
-// 				weekly: 7,
-// 				monthly: 30
-// 			}
+		// 	// This maps the contribution period to the days
+		// 	let periodMap = {
+		// 		daily: 1,
+		// 		weekly: 7,
+		// 		monthly: 30
+		// 	}
 
-// 			// Here we get the contribution period value
-// 			// Then check if the days passed fall under that period
-// 			// If it does we continue to check transactions
-// 			// If it doesn't we set the phase to success and continue as nothing should be done
-// 			let periodCount = periodMap[autoSaveMembers[i]._doc.about.contribution_period]
-// 			if(periodCount && days % periodCount != 0){
-// 				autoSaveMembers[i]._doc.phase = "success"
-// 				continue
-// 			}
-// 		}
+		// 	// Here we get the contribution period value
+		// 	// Then check if the days passed fall under that period
+		// 	// If it does we continue to check transactions
+		// 	// If it doesn't we set the phase to success and continue as nothing should be done
+		// 	let periodCount = periodMap[autoSaveMembers[i]._doc.about.contribution_period]
+		// 	if(periodCount && days % periodCount != 0){
+		// 		autoSaveMembers[i]._doc.phase = "success"
+		// 		continue
+		// 	}
+		// }
 
 
 
