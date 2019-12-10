1a50d7a211b322c6c526858bf19d9f63436b8ebc
iammadab <iammadab@gmail.com>
2019-12-10 12:18:33 +0100

    Implemented the page stat

--- services/page/page.stat.js
+++ services/page/page.stat.js
@@ -1,5 +1,15 @@
-async function stat(req, res, next){
+const pageFunctions = require("./functions")
 
+async function stat(req, res, next){
+	let allMembers = await pageFunctions.fetchUsers()
+	let activeMembers = await pageFunctions.fetchUsers({ stage: "active" })
+
+	req.body.pageData = {
+		allMembersCount: allMembers.length,
+		activeMembersCount: activeMembers.length
+	}
+
+	next()
 }
 
 module.exports = stat
