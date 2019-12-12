c8c98ba0a2db3abe5fe933b0dfc1c4290ba90f93
iammadab <iammadab@gmail.com>
2019-12-12 12:50:00 +0100

    Added logs

--- services/page/page.dashboard.js
+++ services/page/page.dashboard.js
@@ -3,6 +3,7 @@ const pageFunctions = require("./functions")
 async function dashboard(req, res, next){
 	let userObj = await pageFunctions.fetchUser(req.body.user.id)
 	let higherPurchaseTransactions = await pageFunctions.fetchTransactions({ user_id: userObj.user_id, type: "higher_purchase", status: "success" })
+	console.log(higherPurchaseTransactions)
 	let totalPayment = 0, remainingPayment = 0
 
 	if(userObj)
