7b0b4293b410965f4d06ad3ebb617975d4620202
iammadab <iammadab@gmail.com>
2019-12-13 19:08:20 +0100

    Added the sort parameter to fetch transactions

--- services/page/functions/fetchTransactions.js
+++ services/page/functions/fetchTransactions.js
@@ -1,7 +1,7 @@
 const transactionDb = require("../../../data/db/transaction.db")
 
 async function fetchTransactions(query = {}){
-	return transactionDb.findWith(query)
+	return transactionDb.findWith(query, null, -1)
 }
 
 module.exports = fetchTransactions
