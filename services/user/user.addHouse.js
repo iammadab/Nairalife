3a6192a1118a9adb9d64969822093886d25435ea
iammadab <iammadab@gmail.com>
2020-01-19 14:36:14 +0100

    Applied validation to the house

--- services/user/user.addHouse.js
+++ services/user/user.addHouse.js
@@ -1,5 +1,13 @@
-async function addHouse(data){
+const { createValidator } = require("lazy-validator")
 
+const houseValidator = createValidator("address.string, landmark.string")
+
+async function addHouse(data){
+	let validationResult = houseValidator.parse(data)
+	if(validationResult.error)
+		return { status: 400, code: "BAD_REQUEST_ERROR", error: validationResult.errors }
+
+	let validData = validationResult.data
 }
 
 module.exports = addHouse
