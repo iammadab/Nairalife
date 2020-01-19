2ea2912c43a9e96ef3a5be93711451379bf1cb4b
iammadab <iammadab@gmail.com>
2020-01-19 16:05:57 +0100

    Linked the inputs to their database name

--- public/js/custom/guarantor.js
+++ public/js/custom/guarantor.js
@@ -1,1 +1,14 @@
-alert("I am here")
+let store = {
+	guarantorButton: document.querySelector(".guarantor-button"),
+	guarantorFormTag: ".guarantor-form"
+}
+
+;(function attachEvents(){
+	addEvent([store.guarantorButton], "click", submitGuarantor)
+})()
+
+function submitGuarantor(event){
+	event.preventDefault()
+	
+	let guarantorDetails = extractForm(store.guarantorFormTag)
+}
