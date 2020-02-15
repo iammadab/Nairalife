02364aeb8f98c08498977f12150f949b182e26f3
iammadab <iammadab@gmail.com>
2020-02-15 06:11:08 +0100

    Implemented the api for manual payment

--- services/payment/payment.manual.js
+++ services/payment/payment.manual.js
@@ -2,6 +2,7 @@ const { createValidator } = require("lazy-validator")
 
 const verifyPaymentValidator = createValidator("amount.number")
 
+const random = require("../../lib/random")
 const userDb = require("../../data/db/user.db")
 const transactionDb = require("../../data/db/transaction.db")
 
@@ -19,10 +20,29 @@ async function manualPayment(data){
 	if(amount < 0)
 		return { status: 403, code: "NEGATIVE_AMOUNT" }
 
-	let transactionObj = await createTransaction()
+	let reference = await generateReference()
+
+	let transactionObj = await createTransaction(amount, reference, userObj)
+	if(transactionObj)
+		return { status: 200, code: "TRANSACTION_CREATED" }
+
+	return { status: 500, code: "PROBLEM_CREATING_TRANSACTION" }
 }
 
 module.exports = manualPayment
+
+async function generateReference(){
+	let randomReference = random.word(16)
+	if(await isUniqueReference(randomReference))
+		return randomReference
+	else
+		return await generateReference()
+}
+
+async function isUniqueReference(reference){
+	let transactionObj = await transactionDb.findOneWith({ reference })
+	return transactionObj ? false : true
+}
 
 function createTransaction(amount, reference, user){
 	return transactionDb.createTransaction({
