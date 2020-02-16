086fc0eec688132c446b9261ca0461c7321d5589
iammadab <iammadab@gmail.com>
2020-02-16 14:27:50 +0100

    Reloading the page on successful cancel

--- public/js/custom/transaction.js
+++ public/js/custom/transaction.js
@@ -8,5 +8,14 @@ let store = {
 
 function cancelTransaction(event){
 	event.preventDefault()
-	
+
+	let transaction_id = store.cancelButton.dataset.transaction_id
+
+	return api("transaction/decline", { transaction_id: transaction_id, token: getToken() })
+					.then(handleResponse)
+
+	function handleResponse(response){
+		if(response.status == 200)
+			redirect(window.location.href)
+	}
 }
