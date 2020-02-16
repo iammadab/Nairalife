e7f93d97a9343aa3aa70323fa54fe60715ad05a6
iammadab <iammadab@gmail.com>
2020-02-16 14:22:08 +0100

    Appending the transaction id to the cancel button

--- public/js/custom/transaction.js
+++ public/js/custom/transaction.js
@@ -1,1 +1,12 @@
-alert("hello world")
+let store = {
+	cancelButton: document.querySelector(".transaction-cancel")
+}
+
+;(function attachEvent(){
+	addEvent([store.cancelButton], "click", cancelTransaction)
+})()
+
+function cancelTransaction(event){
+	event.preventDefault()
+	
+}
