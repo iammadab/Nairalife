45ee274316803551f194981954dbe1a71e634763
iammadab <iammadab@gmail.com>
2020-02-16 15:02:39 +0100

    Created the boiler plate for cancelling and confirming transaction

--- public/js/custom/admin/payment.js
+++ public/js/custom/admin/payment.js
@@ -1,1 +1,19 @@
-alert("Hello world")
+let store = {
+	cancelButton: document.querySelctor("transaction-cancel"),
+	confirmButton: document.querySelctor("transaction-confirm")
+}
+
+;(function attachEvent(){
+	addEvent([store.cancelButton], "click", cancelTransaction)
+	addEvent([store.confirmButton], "click", confirmTransaction)
+})()
+
+function cancelTransaction(event){
+	event.preventDefault()
+	console.log("Canceling transaction")
+}
+
+function confirmTransaction(event){
+	event.preventDefault()
+	console.log("Confirming transaction")
+}
