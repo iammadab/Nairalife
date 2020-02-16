b0d50248e1c896e009af59f2ce439ea9e5215350
iammadab <iammadab@gmail.com>
2020-02-16 13:30:35 +0100

    Implemented the interface for transaction approval

--- services/transaction/transaction.approve.js
+++ services/transaction/transaction.approve.js
@@ -2,10 +2,26 @@ const { createValidator } = require("lazy-validator")
 
 const transactionApproveValidator = createValidator("transaction_id")
 
+const transactionDb = require("../../data/db/transaction.db")
+const changeStatus = require("./transaction.changeStatus")
+
 async function approveTransaction(data){
 	let validationResult = transactionApproveValidator.parse(data)
 	if(validationResult.error)
 		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
+
+	let { transaction_id } = validationResult.data
+
+	let changeStatusResult = await changeStatus({
+		id: transaction_id,
+		status: "success"
+	})
+	
+	if(changeStatusResult.status != 200)
+		return changeStatusResult
+
+	return { status: 200, code: "TRANSACTION_APPROVED" }
+
 }
 
 module.exports = approveTransaction
