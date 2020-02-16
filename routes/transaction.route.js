0e7d82fb633a6780e8c6a0cae4d9f9704da48894
iammadab <iammadab@gmail.com>
2020-02-16 13:21:16 +0100

    Applied validation to the transaction approval

--- routes/transaction.route.js
+++ routes/transaction.route.js
@@ -7,12 +7,11 @@ const authenticationService = require("../services/authentication")
 const transactionService = require("../services/transaction")
 
 transactionRouter.post(
-		"/approve", 
-		authenticationService.verifyToken("atoken"), 
-		authenticationService.validateAdmin, 
-		bodyResponder(transactionService.approve)
+	"/approve", 
+	authenticationService.verifyToken("atoken"), 
+	authenticationService.validateAdmin, 
+	bodyResponder(transactionService.approve)
 )
-
 transactionRouter.post("/decline", bodyResponder(transactionService.decline))
 
 module.exports = transactionRouter
--- services/transaction/transaction.approve.js
+++ services/transaction/transaction.approve.js
@@ -1,5 +1,11 @@
-async function approveTransaction(data){
+const { createValidator } = require("lazy-validator")
 
+const transactionApproveValidator = createValidator("transaction_id")
+
+async function approveTransaction(data){
+	let validationResult = transactionApproveValidator.parse(data)
+	if(validationResult.error)
+		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
 }
 
 module.exports = approveTransaction
