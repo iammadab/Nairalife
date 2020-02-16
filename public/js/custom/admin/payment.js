43f7ac68cc42870175b5ac65a7620bc8bc2915be
iammadab <iammadab@gmail.com>
2020-02-16 15:11:08 +0100

    Implemented functions to cancel and confirm transactions

--- public/js/custom/admin/payment.js
+++ public/js/custom/admin/payment.js
@@ -10,10 +10,21 @@ let store = {
 
 function cancelTransaction(event){
 	event.preventDefault()
-	console.log("Canceling transaction")
+	let transaction_id = store.cancelButton.dataset.transaction_id
+
+	return api("transaction/decline", { transaction_id: transaction_id, token: getToken("atoken") })
+					.then(handleResponse)
 }
 
 function confirmTransaction(event){
 	event.preventDefault()
-	console.log("Confirming transaction")
+	let transaction_id = store.confirmButton.dataset.transaction_id
+
+	return api("transaction/approve", { transaction_id: transaction_id, atoken: getToken("atoken")})
+					.then(handleResponse)
+}
+
+function handleResponse(response){
+	if(response.status == 200)
+		reload()
 }
