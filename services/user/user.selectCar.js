1922ab2c7e607366b170c2ee164f88b14622bae2
iammadab <iammadab@gmail.com>
2020-02-21 13:22:20 +0100

    Implemented car selection

--- services/user/user.selectCar.js
+++ services/user/user.selectCar.js
@@ -3,6 +3,7 @@ const { createValidator } = require("lazy-validator")
 const selectCarValidator = createValidator("id.string.lowercase")
 
 const carService = require("../car")
+const userPlan = require("./user.plan")
 
 async function selectCar(data){
 	let validationResult = selectCarValidator.parse(data)
@@ -15,7 +16,18 @@ async function selectCar(data){
 	if(carResult.status != 200)
 		return carResult
 
-	console.log(carResult)
+	let { total_amount, period, amount } = carResult.car
+
+	let planResult = await userPlan({
+		total_amount,
+		period,
+		amount,
+		user: data.user
+	})
+	if(planResult.status != 200)
+		return planResult
+
+	return { status: 200, code: "CAR_SELECTED" }
 }
 
 module.exports = selectCar
