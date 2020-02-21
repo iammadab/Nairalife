d8da3ca69584e7c0c81f175db9c5a1fef03fdded
iammadab <iammadab@gmail.com>
2020-02-21 12:11:38 +0100

    Removed the cars from the plan page

---
+++ services/car/car.db.js
@@ -0,0 +1,20 @@
+// Temporary db to contain all cars
+let carsDb = [
+	{
+		name: "Toyota Corrola (2003 - 2006)",
+		total_amount: 2600000,
+		period: 24
+	},
+	{
+		name: "Toyota Camry (2003 - 2005)",
+		total_amount: 2600000,
+		period: 24
+	},
+	{
+		name: "Toyota Matrix (2004 - 2006)",
+		total_amount: 2600000,
+		period: 24
+	}
+]
+
+module.exports = carsDb
