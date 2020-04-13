ccf781fb15d60757c9633f9d5c94fcada47de443
iammadab <iammadab@gmail.com>
2020-04-13 17:53:42 +0100

    Created the loan model

---
+++ data/models/loan.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+
+const loanSchema = mongoose.Schema({
+	user_id: { type: Number, required: true },
+	interest: { type: Number, required: true },
+	initial_amount: { type: Number, required: true },
+	final_amount: { type: Number, required: true },
+	reason: { type: String, required: true },
+	created_at: { type: Date, default: new Date() },
+	approved_at: { type: Date, default: new Date() },
+	started_at: { type: Date, default: new Date() }
+})
+
+const loanModel = mongoose.model("Loan", loanSchema)
+
+module.exports = loanModel
