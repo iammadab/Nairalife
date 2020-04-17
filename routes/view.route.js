c7e97c2385108aa287af92ea45cb3e4a4c31813c
iammadab <iammadab@gmail.com>
2020-04-17 03:30:58 +0100

    Created all the dynamic routing for the loan page

--- routes/view.route.js
+++ routes/view.route.js
@@ -133,9 +133,7 @@ viewRouter.get("/card", cookieNotFound("/login"), verifyToken(), stageRouter("ac
 })
 
 
-viewRouter.get("/loan", cookieNotFound("/login"), verifyToken(), stageRouter("active"), pageService.user, pageService.loan, (req, res) => {
-	res.render("loan", { title: "Instalment Loans", link: "loan",...req.body.pageData })
-})
+viewRouter.get("/loan", cookieNotFound("/login"), verifyToken(), stageRouter("active"), pageService.user, pageService.loan)
 
 
 viewRouter.get("/requests", cookieNotFound("/login"), verifyToken(), stageRouter("active"), pageService.userTransaction, pageService.user, (req, res) => {
--- services/page/page.loan.js
+++ services/page/page.loan.js
@@ -14,6 +14,9 @@ async function loan(req, res, next){
 	else if(userObj.loan_stage == "residence")
 		res.render("residence", { title: "Place of Residence", link: "residence", ...req.body.pageData })
 
+	else if(userObj.loan_stage == "request")
+		res.render("loan", { title: "Instalment Loans", link: "loan",...req.body.pageData })
+
 }
 
 module.exports = loan
