82afb7fabbfe087dd08d38f9726a6ae116e7e90e
iammadab <iammadab@gmail.com>
2020-04-17 19:22:05 +0100

    Created static loan function for generating random reference number

--- data/models/loan.model.js
+++ data/models/loan.model.js
@@ -14,6 +14,16 @@ const loanSchema = mongoose.Schema({
 	started_at: { type: Date }
 })
 
+const { word } = require("../../lib/random")
+loanSchema.statics.getReference = async function referenceLoop(){
+	let possibleReference = word(6)
+	let docExist = await this.find({ reference: possibleReference })
+	if(docExist.length == 0)
+		return possibleReference
+	else
+		return referenceLoop()
+}
+
 const loanModel = mongoose.model("Loan", loanSchema)
 
 module.exports = loanModel
