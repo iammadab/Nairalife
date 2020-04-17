e56d75f9fa5f9dfae9eb91032179b9522f20e02d
iammadab <iammadab@gmail.com>
2020-04-17 19:24:22 +0100

    Passing a reference on loan creation

--- data/models/loan.model.js
+++ data/models/loan.model.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose")
 
 const loanSchema = mongoose.Schema({
 	user_id: { type: Number, required: true },
+	reference: { type: String, required: true },
 	interest: { type: Number, required: true },
 	initial_amount: { type: Number, required: true },
 	final_amount: { type: Number, required: true },
