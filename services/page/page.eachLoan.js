20ae91080baba799e64cda2075bf4cfe0a86c716
iammadab <iammadab@gmail.com>
2020-04-17 14:23:12 +0100

    Displaying the bank name on the request page

--- services/page/page.eachLoan.js
+++ services/page/page.eachLoan.js
@@ -1,12 +1,19 @@
 const pageFunctions = require("./functions")
 const loanDb = require("../../data/db/loan.db")
+const getBank = require("../payment/payment.getBank")
 
 async function eachLoan(req, res, next){
 	let loanObj = await loanDb.findOneWith({ _id: req.params.loan_id })
 	let userObj = await pageFunctions.fetchUser(req.body.user.id)
+	let bankName = ""
 
 	if(userObj.user_id != loanObj.user_id)
 		return res.redirect("/requests")
+
+	let bankInfo = await getBank({ bank_code: userObj.bank[0].account.bank_code })
+	if(bankInfo.status == 200)
+		bankName = bankInfo.bank.name
+
 
 	loanObj._doc.created_at = pageFunctions.createDate(loanObj.created_at).getHypenDate()
 	loanObj._doc.weekly_payment = loanObj.final_amount / loanObj.weeks
@@ -14,7 +21,8 @@ async function eachLoan(req, res, next){
 
 	req.body.pageData = {
 		loan: loanObj,
-		account: userObj.bank[0].account
+		account: userObj.bank[0].account,
+		bankName
 	}
 
 	next()
