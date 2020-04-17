48cab1a1bd2b8db803f56a06f90cf90042fc90bc
iammadab <iammadab@gmail.com>
2020-04-17 19:53:15 +0100

    Added a declined status to the loans along side cancelled

--- data/models/loan.model.js
+++ data/models/loan.model.js
@@ -9,7 +9,7 @@ const loanSchema = mongoose.Schema({
 	reason: { type: String, required: true },
 	weeks: { type: Number, required: true },
 	weeks_before_payment: { type: Number, required: true },
-	status: { type: String, default: "pending", enum: ["pending", "approved", "cancelled", "completed"] },
+	status: { type: String, default: "pending", enum: ["pending", "approved", "cancelled", "declined", "completed"] },
 	created_at: { type: Date, default: new Date() },
 	approved_at: { type: Date },
 	started_at: { type: Date }
--- services/loan/loan.decline.js
+++ services/loan/loan.decline.js
@@ -1,5 +1,32 @@
-async function declineLoan(data){
+const { createValidator } = require("lazy-validator")
 
+const loanDeclineValidator = createValidator("loan_id.string")
+
+const loanDb = require("../../data/db/loan.db")
+const userDb = require("../../data/db/user.db")
+
+async function declineLoan(data){
+	let validationResult = loanDeclineValidator.parse(data)
+	if(validationResult.error)
+		return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.errors }
+
+	let { loan_id } = validationResult.data
+
+	let userObj = await userDb.findOneWith({ _id: data.user.id })
+	if(!userObj)
+		return { status: 403, code: "USER_DOES_NOT_EXIST" }
+
+	let loanObj
+	try{
+		loanObj = await loanDb.findOneWith({ _id: loan_id })
+	} catch(e){
+		return { status: 403, code: "INVALID_LOAN_ID" }
+	}
+
+	if(loanObj.user_id != userObj.user_id)
+		return { status: 403, code: "LOAN_NOT_FOUND" }
+
+	loanObj = await loanDb.appendDoc({ _id: loanObj._id }, "status", "declined")
 }
 
 module.exports = declineLoan
