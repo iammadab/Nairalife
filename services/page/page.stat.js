d99f48e8f9d763888670d12daab380834557c739
iammadab <iammadab@gmail.com>
2020-04-19 04:38:25 +0100

    Showing sufficient data on the admin dashboard

--- services/page/page.stat.js
+++ services/page/page.stat.js
@@ -1,7 +1,9 @@
 const pageFunctions = require("./functions")
-
+const userDb = require("../../data/db/user.db")
+const loanDb = require("../../data/db/loan.db")
 async function stat(req, res, next){
 	let allMembers = await pageFunctions.fetchUsers()
+	let verifiedMembers = await userDb.findWith({ stage: "active" })
 	let activeMembers = await pageFunctions.fetchUsers({ stage: "active", status: "payment_one" })
 	let cars = await pageFunctions.fetchUsers({ status: "payment_two" })
 	let allHpTransactions = await pageFunctions.fetchTransactions({ type: "higher_purchase", status: "success" })
@@ -16,12 +18,30 @@ async function stat(req, res, next){
 		toPay += activeMembers[i]._doc.plan.total_amount || 0
 	}
 
+	let allLoans = await loanDb.findWith({})
+	let pendingLoans = await loanDb.findWith({ status: "pending" })
+	let activeLoans = await loanDb.findWith({ status: "approved" })
+
+	let totalActiveLoanAmount = activeLoans.reduce((acc, curr) => acc + curr.initial_amount, 0)
+	let totalWeeklyLoanPayment = 0
+	for(let i = 0; i < activeLoans.length; i++){
+		let loan = activeLoans[i], weekly = loan.final_amount / loan.weeks
+		totalWeeklyLoanPayment += weekly
+	}
+
 	req.body.pageData = {
 		allMembersCount: allMembers.length,
+		verifiedMembersCount: verifiedMembers.length,
 		activeMembersCount: activeMembers.length,
 		carsCount: cars.length,
 		totalPaid,
-		toPay
+		toPay,
+
+		allLoansCount: allLoans.length,
+		pendingLoansCount: pendingLoans.length,
+		activeLoansCount: activeLoans.length,
+		activeLoansAmount: totalActiveLoanAmount,
+		weeklyLoan: totalWeeklyLoanPayment
 	}
 
 	next()
