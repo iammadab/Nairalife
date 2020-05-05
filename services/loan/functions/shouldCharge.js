1e90a434db128f553d89f59dbe241d49215445bf
iammadab <iammadab@gmail.com>
2020-05-05 13:25:39 +0100

    Implemented the should charge function

---
+++ services/loan/functions/shouldCharge.js
@@ -0,0 +1,29 @@
+async function shouldCharge(period, start, today){
+	start = new Date(start), today = today ? new Date(today) || new Date()
+
+	// Based on the start date and the period, the goal is to determine if the user should be charge on a particular day
+	// First we need to turn the dates to midnight
+	start = start.setHours(0, 0, 0, 0)
+	today = today.setHours(0, 0, 0, 0)
+
+	//Next, we have to figure out how many days has passed since the start and today
+	let daysPassedMilli = Number(today) - Number(start)
+	let days = (new Date(daysPassedMilli)).getDate() - 1 // Sub 1 to adjust for midnight
+
+	let periodMap = {
+		daily: 1,
+		weekly: 7
+	}
+
+	//We use the period map to covert the period to days
+	let periodCount = periodMap[period.toLowerCase()]
+	// Now, we know how many days have passed and the period
+	// If the days passed is a multiple of the period then the user should be charged
+	if(periodCount && days % periodCount != 0)
+		return false
+
+	//If we are here, then the user should be charged
+	return true 
+}
+
+module.exports = shouldCharge
