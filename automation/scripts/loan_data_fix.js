d8711b72194f86ce32f3a69d3e73cd0824a0d571
iammadab <iammadab@gmail.com>
2020-05-05 09:34:48 +0100

    Added the start date to each loan

--- automation/scripts/loan_data_fix.js
+++ automation/scripts/loan_data_fix.js
@@ -3,12 +3,36 @@ require("dotenv").config({ path: path.resolve(__dirname, "../../", ".env") })
 
 const { connectToDb } = require("../../runners/database_runner")
 const loanDb = require("../../data/db/loan.db")
+const { addWeeks } = require("../../lib/date")
 
-connectToDb()
-	.then(getAllLoans)
-	.then(updateDate)
-	.then(console.log)
+// connectToDb()
+	// .then(getApprovedLoans)
+	// .then(addStartDate)
 	.then(exit)
+
+
+async function getApprovedLoans(){
+	return loanDb.findWith({ status: "approved" })
+}
+
+async function addStartDate(loans){
+	// let startDateUpdateStatus = await Promise.all(
+	// 	loans.map(loan => {
+	// 		let startDate = addWeeks(loan.created_at, loan.weeks_before_payment)
+	// 		return loanDb.appendDoc({ _id: loan._id }, "started_at", startDate)
+	// 	})
+	// )
+	console.log(startDateUpdateStatus)
+	
+	loans.forEach(loan => {
+		console.log("Created_at", loan.created_at.toLocaleString())
+		console.log("Weeks before", loan.weeks_before_payment)
+		let startDate = addWeeks(loan.created_at, loan.weeks_before_payment)
+		// console.log(loan.start_date, startDate)
+		console.log("Start date", startDate.toLocaleString())
+		console.log("")
+	})
+}
 
 async function getAllLoans(){
 	let allLoans = await loanDb.findWith({})
