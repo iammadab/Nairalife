0368cd23dd91b0b4805503138b21ab80bba30f85
iammadab <iammadab@gmail.com>
2020-05-05 15:11:10 +0100

    Adding the last payment to the info returned from payment trajectory

--- services/loan/functions/paymentTrajectory.js
+++ services/loan/functions/paymentTrajectory.js
@@ -1,3 +1,5 @@
+const { addDays } = require("../../../lib/date")
+
 function paymentTrajectory(period, start, today){
 	start = new Date(start), today = today ? new Date(today) : new Date()
 
@@ -18,15 +20,16 @@ function paymentTrajectory(period, start, today){
 
 	//We use the period map to covert the period to days
 	let periodCount = periodMap[period.toLowerCase()]
-	// Now, we know how many days have passed and the period
-	// If the days passed is a multiple of the period then the user should be charged
-	if(periodCount && days % periodCount != 0)
-		return false
 
-	//If we are here, then the user should be charged
+	//This figures out the day of the last payment
+	let daysSinceLast = days % periodCount, lastPaymentDuration = days - daysSinceLast
+	let lastPayment = addDays(start, lastPaymentDuration)
+
+
 	return {
 		days,
-		period: periodCount
+		period: periodCount,
+		lastPayment
 	}
 }
 
